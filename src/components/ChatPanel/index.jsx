f8511302e5249c93e7d1bd87dc193a08145877d2
achraffawzi <achraffawzi224a@gmail.com>
2024-04-07 19:01:59 +0000

    fix select dropdown width

--- src/components/ChatPanel/index.jsx
+++ src/components/ChatPanel/index.jsx
@@ -6,7 +6,7 @@ import SearchSection from '../SearchSection';
 
 const ChatPanel = () => {
   return (
-    <div className="h-full chat-panel">
+    <div className="h-full px-3 chat-panel">
       {/* toggler */}
       <Toggler components={[
         <CopilotSection key={0} name="Copilot" />,
