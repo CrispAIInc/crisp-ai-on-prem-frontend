8ae5f603613363a85bf06d6b9dc90e20be54c4fc
achraffawzi <achraffawzi224a@gmail.com>
2024-04-15 13:23:44 +0100

    fix: prevent users from selecting NoData content

--- src/components/NoData/index.jsx
+++ src/components/NoData/index.jsx
@@ -2,7 +2,7 @@ import EmptyData from "../../assets/no-data.svg";
 
 const NoData = () => {
     return (
-        <div className="flex flex-col items-center justify-center gap-4 mx-auto text-center">
+        <div className="flex flex-col items-center justify-center gap-4 mx-auto text-center user-select-none">
             <img src={EmptyData} alt="no data" className="" />
             <h1 className="text-sm text-center text-textColor-200">No data available</h1>
         </div>
