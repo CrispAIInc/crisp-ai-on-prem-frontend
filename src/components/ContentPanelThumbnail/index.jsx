3748c4280fb8e4d89161abad4bf6fac32307ee64
achraffawzi <achraffawzi224a@gmail.com>
2024-04-23 14:29:14 +0100

    fix: Make the same layout for thumbnails as source explorer

--- src/components/ContentPanelThumbnail/index.jsx
+++ src/components/ContentPanelThumbnail/index.jsx
@@ -6,6 +6,8 @@ import PDFThumbnail from "../PDFThumbnail";
 import ImageThumbnail from "../ImageThumbnail";
 import StagedImageThumbnail from '../StagedImageThumbnail';
 import StagedVideoThumbnail from '../StagedVideoThumbnail';
+import { useContext } from 'react';
+import { MainContext } from '../../contexts/mainContext';
 
 const ContentPanelThumbnail = ({
   isDeleting,
@@ -16,40 +18,35 @@ const ContentPanelThumbnail = ({
   onThumbnailClick,
   deleteResource,
 }) => {
+
+  const { theme } = useContext(MainContext);
+
   return (
-    <div className="cursor-pointer 2xl:w-44 2xl:h-2/3" onClick={(event) => onThumbnailClick(event, item)}>
-      <div>
+    <div className={`relative pt-4 rounded-md cursor-pointer ${theme === "dark" ? "!border !border-textColor-300" : '!border !border-textColor-100'} 2xl:w-44 2xl:h-2/3`} onClick={(event) => onThumbnailClick(event, item)}>
+      <>
         {isDeleting && clickedIndex === index ? (
           <div className="thumbnail-loader">
             <LoadingSpinner />
           </div>
         ) : null}
-        <div
-
-          className="relative"
-        >
-          <Checkbox
-            className="!absolute top-0 -left-1"
-            checked={item.is_selected}
-            onChange={() => handleCheckboxChange(item)}
-            onClick={(event) => event.stopPropagation()}
-            inputProps={{ "aria-label": "Select source" }}
-          />
-          {item.file_type === "video" && <StagedVideoThumbnail item={item} />}
-          {item.file_type === "pdf" && <PDFThumbnail item={item} />}
-          {item.file_type === "img" && <StagedImageThumbnail item={item} />}
-          <DeleteIcon
-            onClick={(event) => { event.stopPropagation(); deleteResource(event, index); }}
-            color="error"
-            className="absolute cursor-pointer left-1 bottom-1"
-          />
-        </div>
-      </div>
-      {/* <span className="text-xs leading-none break-words text-textColor-200">
-        {item.source_path}
-      </span> */}
+        <Checkbox
+          className="!absolute top-0 -left-1 p-0 !ml-1"
+          checked={item.is_selected}
+          onChange={() => handleCheckboxChange(item)}
+          onClick={(event) => event.stopPropagation()}
+          inputProps={{ "aria-label": "Select source" }}
+        />
+        {item.file_type === "video" && <StagedVideoThumbnail item={item} />}
+        {item.file_type === "pdf" && <PDFThumbnail item={item} />}
+        {item.file_type === "img" && <StagedImageThumbnail item={item} />}
+        <DeleteIcon
+          onClick={(event) => { event.stopPropagation(); deleteResource(event, index); }}
+          color="error"
+          className="absolute cursor-pointer !top-0 !left-[calc(100%-24px)] mr-1"
+        />
+      </>
     </div>
   );
 };
 
-export default ContentPanelThumbnail;
+export default ContentPanelThumbnail;;
