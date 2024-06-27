fab3616b4eafacdc384dd4bdae4d4c64f071f8c5
achraffawzi <achraffawzi224a@gmail.com>
2024-06-27 23:24:04 +0100

    FIX: Prevent preview image from disappearing upon click

--- src/components/PreviewModal/index.jsx
+++ src/components/PreviewModal/index.jsx
@@ -1,13 +1,25 @@
 function PreviewModal({ closeLightbox, content }) {
+
+    const handleCloseButtonClick = (e) => {
+        e.stopPropagation();
+        closeLightbox();
+    };
+
+    const handleOutsideClick = (e) => {
+        if (e.target === e.currentTarget) {
+            closeLightbox();
+        }
+    };
+
     return (
         <div
             className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-75"
-            onClick={closeLightbox} // Close on click outside or click on lightbox
+            onClick={(e) => handleOutsideClick(e)} // Close on click outside or click on lightbox
         >
             <div className="relative w-96 h-96 max-w-[90vw] max-h-[90vh]"> {/* Wrap lightbox content */}
                 <button
                     className="absolute text-2xl text-primary-300 top-4 right-4"
-                    onClick={closeLightbox} // Close on button click
+                    onClick={(e) => handleCloseButtonClick(e)} // Close on button click
                 >
                     &times;
                 </button>
