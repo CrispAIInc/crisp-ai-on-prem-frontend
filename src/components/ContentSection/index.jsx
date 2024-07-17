806e67f8ca6c1bdf1ac8af1dd9a4fe88d96b23a3
achraffawzi <achraffawzi224a@gmail.com>
2024-07-17 16:13:14 +0100

    style: use grid for staged sources

--- src/components/ContentSection/index.jsx
+++ src/components/ContentSection/index.jsx
@@ -308,7 +308,7 @@ const ContentSection = ({
             <BaseHeading text='Selected sources' />
 
             {
-                <div className={`flex flex-col items-center w-4/5 w-full max-w-full gap-8 mx-auto mt-4 overflow-y-auto ${theme === 'dark' ? '!border !border-textColor-300' : 'border'} empty:!border-none`}>
+                <div className={`grid grid-cols-[repeat(auto-fill,_160px)] gap-5 justify-center items-start w-4/5 w-full max-w-full gap-8 mx-auto mt-4 overflow-y-auto ${theme === 'dark' ? '!border !border-textColor-300' : 'border'} empty:!border-none`}>
                     {knowledgeBase.slice(0).reverse().map((item, index) => {
                         if (canRenderSourceThumbnail(item)) {
                             return (<ContentPanelThumbnail
