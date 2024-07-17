1abe814c58e9d17809663c6c6953a1d5d1ddbf70
achraffawzi <achraffawzi224a@gmail.com>
2024-07-17 16:25:16 +0100

    style: set max width to 100% for insight SideCard

--- src/layouts/SideCard.jsx
+++ src/layouts/SideCard.jsx
@@ -5,7 +5,7 @@ function SideCard({ children, onClick }) {
     const { theme } = useContext(MainContext);
     return (
         <div
-            className={`p-2 ${theme === 'light' ? 'bg-white' : 'bg-background_workspace'} rounded-md shadow-[0_0px_8px_0px_rgba(0,0,0,0.15)] cursor-pointer user-select-none max-w-96`}
+            className={`p-2 ${theme === 'light' ? 'bg-white' : 'bg-background_workspace'} rounded-md shadow-[0_0px_8px_0px_rgba(0,0,0,0.15)] cursor-pointer user-select-none max-w-full`}
             onClick={onClick}
         >
             {children}
