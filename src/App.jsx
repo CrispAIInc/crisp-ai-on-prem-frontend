848ebc183828f9d04c8ad52b81f43b25ce66d89d
achraffawzi <achraffawzi224a@gmail.com>
2024-08-26 13:09:28 +0100

    update landingpage

--- src/App.jsx
+++ src/App.jsx
@@ -44,8 +44,8 @@ function App() {
     <div className={`App ${theme}`}>
       <Router>
         <Routes>
-          <Route path="/" element={<HomePage theme={theme} />} />
-          <Route path="/workspace" element={<MainWorkspacePage theme={theme} />} />
+          <Route path="/landing-page" element={<HomePage theme={theme} />} />
+          <Route path="/" element={<MainWorkspacePage theme={theme} />} />
         </Routes>
       </Router>
     </div>
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,8 +1,6 @@
-import BaseHeading from '../components/BaseHeading';
-
 export default function HomePage({ theme }) {
     return (
-        <div className="h-full bg-background_workspace main-workspace-container">
+        <div className="flex flex-col items-center justify-center h-full bg-background_workspace main-workspace-container ">
             {/* logo */}
             <div className="flex flex-col items-center justify-center mt-2">
                 <img src="/app-logo.svg" alt="logo" className="w-24 h-24" width="96" height="96" />
@@ -11,12 +9,17 @@ export default function HomePage({ theme }) {
             <p className={`${theme === "light"
                 ? "text-textColor-300"
                 : "text-textColor-100"
-                } text-center text-2xl mt-10 w-[600px] max-w-[90vw] mx-auto !my-10`}>First of its kind platform for Creation of interactive Stories based on facts from your Multi-Modal Knowledge Base.</p>
+                } text-center text-2xl font-bold w-[600px] max-w-[90vw] mx-auto !mt-5`}><span className="text-primary-300">C</span>reation of <span className="text-primary-300">R</span>ooted <span className="text-primary-300">I</span>nteractive <span className="text-primary-300">S</span>tories <span className="text-primary-300">P</span>latform.</p>
 
             <p className={`${theme === "light"
                 ? "text-textColor-300"
                 : "text-textColor-100"
-                } text-center text-lg font-bold mt-10 w-[600px] max-w-[90vw] mx-auto`}>Stay tuned for details.</p>
+                } text-center text-2xl font-semibold !mb-20 w-[600px] max-w-[90vw] mx-auto !mt-5`}>Interctive story creation made simple, efficient and accurate via Multi-Modal content discovery.</p>
+
+            <p className={`${theme === "light"
+                ? "text-textColor-300"
+                : "text-textColor-100"
+                } text-center text-lg font-bold mt-10 w-[600px] max-w-[90vw] mx-auto`}>Stay tuned for details...</p>
 
         </div>
     );
