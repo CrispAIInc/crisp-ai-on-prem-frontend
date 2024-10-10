3d16cdafe8fde15ae31cf2e61d5754e4f05e9661
achraffawzi <achraffawzi224a@gmail.com>
2024-10-10 11:05:53 +0100

    decrease icon size in staged sources thumbnails

--- src/components/StagedVideoThumbnail/index.jsx
+++ src/components/StagedVideoThumbnail/index.jsx
@@ -21,8 +21,8 @@ function StagedVideoThumbnail({ item }) {
                     alt="Video Thumbnail" />
 
                 {/* thumbnail bottom title */}
-                <div className={`absolute bottom-0 left-0 flex items-center w-full h-8  truncate ${theme === 'light' ? 'bg-white' : 'bg-[#333333]'}`}>
-                    <PlayCircleIcon style={{ color: `${theme === 'light' ? '#333' : '#ABAEB4'}` }} />
+                <div className={`absolute bottom-0 left-0 flex items-center w-full h-8 gap-1 truncate ${theme === 'light' ? 'bg-white' : 'bg-[#333333]'}`}>
+                    <PlayCircleIcon fontSize="15" style={{ color: `${theme === 'light' ? '#333' : '#ABAEB4'}` }} />
                     <p className={`${theme === "light" ? 'text-black' : 'text-white'} text-sm truncate`}>{item.source_path}</p>
                 </div>
             </div>
