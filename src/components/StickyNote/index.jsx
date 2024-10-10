ad5ac5644a0941321a134aa7bccc510a6e37298c
achraffawzi <achraffawzi224a@gmail.com>
2024-10-10 10:43:43 +0100

    make sticky notes smaller

--- src/components/StickyNote/index.jsx
+++ src/components/StickyNote/index.jsx
@@ -65,7 +65,7 @@ const StickyNote = ({ index, setNoteIndex, note }) => {
 
     return (
         <div
-            className={`p-1 rounded-lg w-36 h-36 relative transform shadow-[rgba(0,0,15,0.5)_0px_8px_19px_-10px] flex flex-col cursor-pointer`}
+            className={`p-1 rounded-lg w-32 h-32 relative transform shadow-[rgba(0,0,15,0.5)_0px_8px_19px_-10px] flex flex-col cursor-pointer`}
             style={{
                 backgroundColor: color,
                 // boxShadow: '0 15px 25px rgba(0, 0, 0, 0.2)', // Bottom-only shadow
