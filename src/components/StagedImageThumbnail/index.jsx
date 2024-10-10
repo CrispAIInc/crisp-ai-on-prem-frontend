af371b9aebf451f3c216be9f678ff2ee92b36560
achraffawzi <achraffawzi224a@gmail.com>
2024-10-10 11:01:47 +0100

    add truncate to caption tooltip of staged image thumbnails

--- src/components/StagedImageThumbnail/index.jsx
+++ src/components/StagedImageThumbnail/index.jsx
@@ -11,7 +11,7 @@ function StagedImageThumbnail({ item }) {
     const { theme } = useContext(MainContext);
 
     const renderTooltip = props => (
-        <Tooltip className='truncate tooltip h-80' {...props}>{item.caption}</Tooltip>
+        <Tooltip className='h-auto truncate tooltip' {...props}>{item.caption.split(' ').slice(0, 30).join(' ')}...</Tooltip>
     );
 
     return (
