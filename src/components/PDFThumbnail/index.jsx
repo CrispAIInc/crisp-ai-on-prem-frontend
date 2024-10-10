518e202e159220be4ac78c9758b5c380b27461e0
achraffawzi <achraffawzi224a@gmail.com>
2024-10-10 11:05:14 +0100

    decrease icon size in staged sources thumbnails

--- src/components/PDFThumbnail/index.jsx
+++ src/components/PDFThumbnail/index.jsx
@@ -22,9 +22,9 @@ function PDFThumbnail({ item }) {
           alt="PDF Thumbnail" />
 
         {/* thumbnail bottom title */}
-        <div className={`absolute bottom-0 left-0 flex items-center w-full h-8  truncate ${theme === 'light' ? 'bg-white' : 'bg-[#333333]'}`}>
-          <InsertDriveFileIcon style={{ color: `${theme === 'light' ? '#333' : '#ABAEB4'}` }} />
-          <p className={`${theme === "light" ? 'text-black' : 'text-white'} text-sm truncate`}>{item.source_path}</p>
+        <div className={`absolute bottom-0 left-0 flex items-center w-full h-8 gap-1 truncate ${theme === 'light' ? 'bg-white' : 'bg-[#333333]'}`}>
+          <InsertDriveFileIcon fontSize="15" style={{ color: `${theme === 'light' ? '#333' : '#ABAEB4'}` }} />
+          <p className={`${theme === "light" ? 'text-black' : 'text-white'} text-xs truncate`}>{item.source_path}</p>
         </div>
       </div>
     </OverlayTrigger>
