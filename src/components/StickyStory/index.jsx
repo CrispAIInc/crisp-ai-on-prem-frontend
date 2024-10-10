88f970effea40e0b5b1a98727d2aa447d7043611
achraffawzi <achraffawzi224a@gmail.com>
2024-10-10 10:44:23 +0100

    make sticky stories smaller

--- src/components/StickyStory/index.jsx
+++ src/components/StickyStory/index.jsx
@@ -27,7 +27,7 @@ function StickyStory({ story }) {
 
     return (
         <div
-            className={`p-1 rounded-lg w-36 h-36 relative transform shadow-[rgba(0,0,15,0.5)_0px_8px_19px_-10px] flex flex-col cursor-pointer`}
+            className={`p-1 rounded-lg w-32 h-32 relative transform shadow-[rgba(0,0,15,0.5)_0px_8px_19px_-10px] flex flex-col cursor-pointer`}
             style={{
                 backgroundColor: color,
                 transform: `rotate(${Math.random() * 6 - 3}deg)`,
