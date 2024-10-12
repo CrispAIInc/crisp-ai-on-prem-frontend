143dc8b733c1a46c56a510d64980eb378c283580
achraffawzi <achraffawzi224a@gmail.com>
2024-10-12 10:11:19 +0100

    make ModelChip paddings smaller

--- src/components/ModelChip/index.jsx
+++ src/components/ModelChip/index.jsx
@@ -1,6 +1,6 @@
 const ModelChip = ({ modelName }) => {
     return (
-        <span className={`rounded-full py-[2px] px-[5px] font-bold border-none text-${modelName}-200 bg-${modelName}-100 text-xs`}>{modelName}</span>
+        <span className={`rounded-full py-[1px] px-[3px] font-bold border-none text-${modelName}-200 bg-${modelName}-100 text-xs`}>{modelName}</span>
     );
 };
 
