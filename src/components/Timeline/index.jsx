e171aeb159c2a70a2a3caf27a38a5c2e870e74f7
achraffawzi <achraffawzi224a@gmail.com>
2024-12-02 19:33:01 +0100

    add arrows for each timeline item pseudo element

--- src/components/Timeline/index.jsx
+++ src/components/Timeline/index.jsx
@@ -42,12 +42,13 @@ function Timeline({ theme }) {
         <div className="main  overflow-y-auto overflow-x-hidden relative  m-auto w-6/12  max-w-[90vw] ">
             {/* <div className="sticky top-0 left-0 right-0 z-20 h-8 pointer-events-none bg-gradient-to-b from-background to-transparent"></div> */}
 
-            <div className="relative w-full timeline before:absolute before:rounded-lg before:top-0 before:left-1/2 before:w-2 before:h-full before:bg-primary-300">
+            <div className="relative w-full timeline before:absolute before:rounded-lg before:top-0 before:left-1/2 before:w-1 before:h-full before:bg-primary-300">
                 {
                     chapters.map((chapter, index) => (
                         <>
-                            <div className={`timeline-item relative w-1/2 p-2 ${index % 2 === 0 ? 'left-0 before:right-[-13px]' : 'left-1/2 text-left before:left-[-14px] ml-[9px]'} before:absolute before:top-1/2 before:-translate-y-1/2  before:w-5 before:h-5 before:bg-primary-200 before:border before:rounded-full before:z-10`}>
+                            <div className={`timeline-item relative w-1/2 p-2 ${index % 2 === 0 ? 'left-0 before:right-[0px] before:border-r-blue-500  before:border-r-8' : 'left-1/2 text-left before:left-[-6px] ml-[9px] before:border-l-8 before:border-l-blue-500'} before:absolute before:top-1/2 before:-translate-y-1/2  before:w-0 before:h-0  before:border before:border-t-8 before:border-b-8  before:border-transparent before:z-10`}>
                                 <div className={`relative grid md:grid-cols-[30%,1fr] gap-3 p-3 bg-background rounded-md shadow-md content`}>
+                                    {/* <div className="absolute right-0 w-0 h-0 mr-4 border-t-8 border-b-8 border-r-8 border-transparent border-r-blue-500"></div> */}
                                     <div className="w-full rounded-md min-w-2/6">
                                         <img src={chapter.img} alt="chapter" className="object-cover w-full h-full rounded-md" />
                                     </div>
