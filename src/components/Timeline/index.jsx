765ae56d92feaa44f6f49c5cfe210251a2a84236
achraffawzi <achraffawzi224a@gmail.com>
2024-12-11 12:51:12 +0100

    add overlay to show description in timeline chapters

--- src/components/Timeline/index.jsx
+++ src/components/Timeline/index.jsx
@@ -78,11 +78,11 @@ function Timeline({ theme, chapters }) {
             <div className="relative w-full timeline before:absolute before:rounded-lg before:top-0 before:left-1/2 before:w-1 before:h-full before:bg-primary-300">
                 {
                     chapters.map((chapter, index) => (
-                        <div key={index} className={`flex gap-4 p-2 h-36 ${index % 2 === 0 && 'flex-row-reverse'}`}>
+                        <div key={index} className={`flex gap-4 p-2 h-36 ${index % 2 !== 0 && 'flex-row-reverse'}`}>
                             {/* chapter description */}
-                            {<p className={`${index % 2 === 0 && 'left-1/2'} w-1/2 h-full p-2 overflow-y-auto border ${theme === 'dark' && 'text-textColor-100 '}`}>lorem ipsum dolor sit amet consectetur adipisicing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua lorem ipsum dolor sit amet consectetur adipisicing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua lorem ipsum dolor sit amet consectetur adipisicing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua  lorem ipsum dolor sit amet consectetur adipisicing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua</p>}
+                            {/* {<p className={`${index % 2 === 0 && 'left-1/2'} w-1/2 h-full p-2 overflow-y-auto border ${theme === 'dark' && 'text-textColor-100 '}`}>lorem ipsum dolor sit amet consectetur adipisicing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua lorem ipsum dolor sit amet consectetur adipisicing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua lorem ipsum dolor sit amet consectetur adipisicing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua  lorem ipsum dolor sit amet consectetur adipisicing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua</p>} */}
                             {/* chapter card */}
-                            <div className={`timeline-item relative w-1/2 ${index % 2 === 0 ? 'left-0 before:right-[-11px] before:border-r-blue-500  before:border-r-8' : 'text-left before:left-[-12px] ml-[9px] before:border-l-8 before:border-l-blue-500'} before:absolute before:top-1/2 before:-translate-y-1/2  before:w-0 before:h-0  before:border before:border-t-8 before:border-b-8  before:border-transparent before:z-10`}>
+                            <div className={`timeline-item relative w-1/2 !px-5 ${index % 2 === 0 ? 'left-0 before:right-[0px] before:border-r-blue-500  before:border-r-8' : 'text-left before:left-[4px] ml-[9px] before:border-l-8 before:border-l-blue-500'} before:absolute before:top-1/2 before:-translate-y-1/2  before:w-0 before:h-0  before:border before:border-t-8 before:border-b-8  before:border-transparent before:z-10`}>
                                 <div className={`relative grid md:grid-cols-[30%,1fr] gap-3 p-3 bg-background rounded-md shadow-md content`}>
                                     {/* <div className="absolute right-0 w-0 h-0 mr-4 border-t-8 border-b-8 border-r-8 border-transparent border-r-blue-500"></div> */}
                                     <div className="w-full rounded-md min-w-2/6">
@@ -98,13 +98,12 @@ function Timeline({ theme, chapters }) {
                                             <AccessTimeIcon size="small" /> {chapter.timestamps[0]} - {chapter.timestamps[1]}
                                         </h5>
                                         <h5 className="mb-0 text-lg font-semibold line-clamp-1">{chapter.title}</h5>
-                                        <OverlayTrigger className='tooltip' placement="right" overlay={(props) => renderTooltip(props, chapter.description)}>
+                                        <OverlayTrigger className='tooltip' placement={index % 2 === 0 ? 'bottom' : "bottom"} overlay={(props) => renderTooltip(props, chapter.description)}>
                                             <p className="text-xs truncate line-clamp-3 text-wrap" onMouseEnter={() => setHoveredChapterId(chapter.id)} onMouseOver={() => setHoveredChapterId(-1)}>{chapter.description}</p>
                                         </OverlayTrigger>
                                     </div>
                                 </div>
                             </div>
-                            {/* {index % 2 === 0 && <p className={`w-1/2 h-full p-2 overflow-y-auto border  ${theme === 'dark' && 'text-textColor-100 '}`}>lorem ipsum dolor sit amet consectetur adipisicing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua lorem ipsum dolor sit amet consectetur adipisicing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua lorem ipsum dolor sit amet consectetur adipisicing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua  lorem ipsum dolor sit amet consectetur adipisicing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua</p>} */}
                         </div>
                     ))
                 }
