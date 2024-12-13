8e7c5b8d7ab2d87c4234e316ad6abb14c5e02011
achraffawzi <achraffawzi224a@gmail.com>
2024-12-13 22:01:54 +0100

    increase logo size in landing page

--- src/components/Hero/index.jsx
+++ src/components/Hero/index.jsx
@@ -19,44 +19,44 @@ export default function Hero() {
                     } text-center text-5xl font-semibold  w-[900px] max-w-[90vw] mx-auto`}>Interactive story creation made simple, efficient and accurate via <span className='text-transparent bg-clip-text bg-gradient-to-r from-blue-500 to-violet-500'>Multi-Modal</span> content discovery.</p> */}
 
                 <div className='flex items-center gap-2'>
-                    <img src="/crisp-ai-logo.png" alt="Crisp AI - Interactive Story and Report Creation Platform" className="w-16 h-16" width="64" height="64" />
-                    <p className={`font-sans text-3xl font-extrabold text-center user-select-none ${theme === 'light' ? 'text-textColor-300' : 'text-textColor-100'}`}>Crisp AI</p>
+                    <img src="/crisp-ai-logo.png" alt="Crisp AI - Interactive Story and Report Creation Platform" className="w-24 h-24" width="64" height="64" />
+                    <p className={`font-sans text-5xl font-extrabold text-center user-select-none ${theme === 'light' ? 'text-textColor-300' : 'text-textColor-100'}`}>Crisp AI</p>
                 </div>
 
                 <h2 className={`z-1 ${theme === "light"
                     ? "text-textColor-300"
                     : "text-white"
-                    } text-center text-5xl font-semibold  w-[900px] max-w-[90vw] mx-auto`}>Did you know that 80% of global enterprise data exists in the form of videos?</h2>
+                    } text-center text-3xl font-semibold  w-[900px] max-w-[90vw] mx-auto`}>Did you know that 80% of global enterprise data exists in the form of videos?</h2>
 
                 <p className={`z-1 ${theme === "light"
                     ? "text-textColor-300"
                     : "text-white"
-                    } text-center text-xl  w-[900px] max-w-[90vw] mx-auto`}>Yet, only a small fraction of this valuable resource is discoverable today. Why? Because manual labeling processes and even automated approaches that rely solely on audio fall short—they fail to harness the full potential of video data, overlooking the critical visual context. With hundreds of petabytes of videos out there, it&apos;s time for a transformative solution.</p>
+                    } text-center text-lg  w-[900px] max-w-[90vw] mx-auto`}>Yet, only a small fraction of this valuable resource is discoverable today. Why? Because manual labeling processes and even automated approaches that rely solely on audio fall short—they fail to harness the full potential of video data, overlooking the critical visual context. With hundreds of petabytes of videos out there, it&apos;s time for a transformative solution.</p>
 
                 <p className={`z-1 ${theme === "light"
                     ? "text-textColor-300"
                     : "text-white"
-                    } text-center text-xl  w-[900px] max-w-[90vw] mx-auto`}>At <span className="font-bold">Crisp AI</span>, we&apos;re harnessing the power of Generative AI to unlock the true potential of video data. Our proprietary technology not only automates the discovery process but does so by integrating both audio and visual elements—making it faster, smarter, and cost-effective.</p>
+                    } text-center text-lg  w-[900px] max-w-[90vw] mx-auto`}>At <span className="font-bold">Crisp AI</span>, we&apos;re harnessing the power of Generative AI to unlock the true potential of video data. Our proprietary technology not only automates the discovery process but does so by integrating both audio and visual elements—making it faster, smarter, and cost-effective.</p>
 
                 <p className={`z-1 ${theme === "light"
                     ? "text-textColor-300"
                     : "text-white"
-                    } text-center text-xl  w-[900px] max-w-[90vw] mx-auto`}>But we don&apos;t stop there. Once video data is discoverable, we take it a step further. Crisp AI combines text, visual, and image data  of enterprises to craft compelling narratives and insightful reports—available in 114 languages. This is more than data accessibility; it&apos;s about turning unstructured data into impactful, enterprise-ready stories.</p>
+                    } text-center text-lg  w-[900px] max-w-[90vw] mx-auto`}>But we don&apos;t stop there. Once video data is discoverable, we take it a step further. Crisp AI combines text, visual, and image data  of enterprises to craft compelling narratives and insightful reports—available in 114 languages. This is more than data accessibility; it&apos;s about turning unstructured data into impactful, enterprise-ready stories.</p>
 
                 <p className={`z-1 ${theme === "light"
                     ? "text-textColor-300"
                     : "text-white"
-                    } text-center text-xl  w-[900px] max-w-[90vw] mx-auto`}>We&apos;re on a mission to revolutionize how organizations leverage their vast video and multimodal data, enabling powerful and engaging storytelling at scale.</p>
+                    } text-center text-lg  w-[900px] max-w-[90vw] mx-auto`}>We&apos;re on a mission to revolutionize how organizations leverage their vast video and multimodal data, enabling powerful and engaging storytelling at scale.</p>
 
                 <p className={`z-1 ${theme === "light"
                     ? "text-textColor-300"
                     : "text-white"
-                    } text-center text-xl  w-[900px] max-w-[90vw] mx-auto`}>We&apos;re inviting select strategic partners to join us in this journey of discovery and transformation. Together, we can unlock new insights, drive innovation, and shape the future of enterprise data.</p>
+                    } text-center text-lg  w-[900px] max-w-[90vw] mx-auto`}>We&apos;re inviting select strategic partners to join us in this journey of discovery and transformation. Together, we can unlock new insights, drive innovation, and shape the future of enterprise data.</p>
 
                 <p className={`z-1 ${theme === "light"
                     ? "text-textColor-300"
                     : "text-white"
-                    } text-center text-xl  w-[900px] max-w-[90vw] mx-auto`}>Are you ready to make an impact? Let&apos;s connect.</p>
+                    } text-center text-lg  w-[900px] max-w-[90vw] mx-auto`}>Are you ready to make an impact? Let&apos;s connect.</p>
 
 
                 {/* <p className={`z-1 ${theme === "light"
