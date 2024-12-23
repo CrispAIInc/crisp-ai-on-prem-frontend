38184f9f06e1be6e7c9952f495958efba213c281
achraffawzi <achraffawzi224a@gmail.com>
2024-12-23 10:54:01 +0100

    fix height for app and timeline component

--- src/components/Timeline/index.jsx
+++ src/components/Timeline/index.jsx
@@ -53,12 +53,12 @@ function Timeline({ theme, chapters, workspaceContainer }) {
             <div className="relative w-full timeline before:absolute before:rounded-lg before:top-0 before:left-1/2 before:w-1 before:h-full before:bg-primary-300">
                 {
                     chapters.map((chapter, index) => (
-                        <div key={chapter.id} className={`flex gap-4 p-2 h-36 ${index % 2 !== 0 && 'flex-row-reverse'}`}>
+                        <div key={chapter.id} className={`flex gap-4 p-2 h-auto ${index % 2 !== 0 && 'flex-row-reverse'}`}>
                             {/* chapter card */}
                             <div className={`timeline-item relative w-1/2 !px-5 ${index % 2 === 0 ? 'left-0 before:right-[0px] before:border-r-blue-500  before:border-r-8' : 'text-left before:left-[4px] ml-[9px] before:border-l-8 before:border-l-blue-500'} before:absolute before:top-1/2 before:-translate-y-1/2  before:w-0 before:h-0  before:border before:border-t-8 before:border-b-8  before:border-transparent before:z-10`}>
-                                <div className={`relative grid md:grid-cols-[30%,1fr] items-center gap-3 p-3 bg-background rounded-md shadow-md content`}>
+                                <div className={`relative grid md:grid-cols-[30%,1fr] gap-3 p-3 bg-background rounded-md shadow-md content`}>
                                     {/* <div className="absolute right-0 w-0 h-0 mr-4 border-t-8 border-b-8 border-r-8 border-transparent border-r-blue-500"></div> */}
-                                    <div className="w-full rounded-md h-fit">
+                                    <div className="w-full rounded-md ">
                                         <img src={import.meta.env.VITE_API_ENDPOINT + (chapter.keyframe_url ?? chapter.thumbnail_url)} alt="chapter" className="object-cover w-full h-full rounded-md" />
                                     </div>
                                     <div className={`flex flex-col gap-0 ${theme === 'dark' ? 'text-textColor-100' : " text-textColor-300"}`}>
