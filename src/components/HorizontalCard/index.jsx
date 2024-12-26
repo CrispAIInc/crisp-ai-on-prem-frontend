8330bfd0639c8a7a63812d302a86a84c8a0a3a3c
achraffawzi <achraffawzi224a@gmail.com>
2024-12-26 12:29:13 +0100

    increase width of thumbnail preview image component

--- src/components/HorizontalCard/index.jsx
+++ src/components/HorizontalCard/index.jsx
@@ -31,7 +31,7 @@ function HorizontalCard({ item, workspaceContainer }) {
                 <img src={thumbnail} alt="chapter" className="object-cover w-full h-full rounded-md" />
             </div>
             {isLightboxOpen && (
-                <PreviewModal closeLightbox={closeLightbox} content={thumbnail} classNames="w-1/3 h-full" />
+                <PreviewModal closeLightbox={closeLightbox} content={thumbnail} classNames="!w-1/3 h-full" />
             )}
             {/* item content */}
             <div>
