3163196cb6d037b7d91ca45693c54e2832ccc60f
achraffawzi <achraffawzi224a@gmail.com>
2024-12-29 20:26:09 +0100

    fix preview width for smaller devices

--- src/components/TimelineItem/index.jsx
+++ src/components/TimelineItem/index.jsx
@@ -27,7 +27,7 @@ function TimelineItem({ chapter, index, theme, workspaceContainer, handleReadMor
                     <img src={thumbnail} alt="chapter" className="object-cover w-full h-full rounded-md" />
                 </div>
                 {isLightboxOpen && (
-                    <PreviewModal closeLightbox={closeLightbox} content={thumbnail} classNames="!w-1/3 h-full" />
+                    <PreviewModal closeLightbox={closeLightbox} content={thumbnail} classNames="w-full sm:w-1/2 lg:w-1/3 h-full" />
                 )}
                 <div className={`flex flex-col gap-0 ${theme === 'dark' ? 'text-textColor-100' : " text-textColor-300"}`}>
                     {
