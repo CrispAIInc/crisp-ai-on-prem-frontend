46ec6bddb03f237b0e387da9c2a053c3ad055f8c
achraffawzi <achraffawzi224a@gmail.com>
2025-03-09 19:42:54 +0000

    remove sources with no embeddings from updating

--- src/components/CopilotSection/index.jsx
+++ src/components/CopilotSection/index.jsx
@@ -101,7 +101,7 @@ const CopilotSection = ({ chatLoaded, setChatLoaded, sidebarWidth }) => {
         `/chat/${selectedCategoryChat}`,
         "post",
         JSON.stringify({
-          sources: selectedSources,
+          sources: selectedSources?.filter(item => item?.metadata?.embeddings_generated),
           category: selectedCategoryChat,
           selectedAll,
           is_exclusive: Boolean(sourcesWithExclusive?.find(item => item === currentResource?.source_path)?.length)
