eb7defbb707ccb8cd88411b5024338cd2bd74198
achraffawzi <achraffawzi224a@gmail.com>
2025-03-27 14:22:17 +0000

    remove flex-1 from speaker element to prevent transcription content from being pushed to the right

--- src/components/MetadataPanel/index.jsx
+++ src/components/MetadataPanel/index.jsx
@@ -586,7 +586,7 @@ const MetadataPanel = ({ workspaceContainer }) => {
                           {
                             translatedResource?.transcription?.content?.map((topic, index) => (
                               <div key={index} className="flex items-baseline gap-3">
-                                <div className='flex-1'>
+                                <div className=''>
                                   <h4 className='text-[16px] font-semibold '>{topic.speaker?.toLowerCase()}: </h4>
                                   <div className="flex items-center gap-2 cursor-pointer" onClick={() => {
                                     setCurrentResource(prev => ({ ...prev, timestamp: topic.start_time }));
