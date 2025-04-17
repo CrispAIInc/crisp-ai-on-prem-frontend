147b2b4758d62c56c7ee8424cc384ad5993a2bba
achraffawzi <achraffawzi224a@gmail.com>
2025-04-17 22:20:03 +0100

    set same dimensions for highlight thumbnails

--- src/components/HorizontalCard/index.jsx
+++ src/components/HorizontalCard/index.jsx
@@ -16,10 +16,10 @@ function HorizontalCard({ item, workspaceContainer }) {
     const thumbnail = import.meta.env.VITE_API_ENDPOINT + (item.keyframe_url ?? item.thumbnail_url);
 
     return (
-        <div key={item.id} className={`relative flex gap-3 p-3 bg-background rounded-md shadow-sm sm:w-2/3 md:w-[100%] mb-4`}>
+        <div key={item.id} className={`relative grid grid-cols-[64px_1fr] gap-3 p-3 bg-background rounded-md shadow-sm mb-4`}>
             {/* item thumbnail */}
-            <div className="relative w-full h-full rounded-md cursor-pointer min-w-2/6 lg:w-1/3" onClick={() => setLightboxOpen(true)}>
-                <img src={thumbnail} alt="chapter" className="object-cover w-full h-full max-w-sm rounded-md lg:max-w-full" />
+            <div className="relative w-20 h-20 bg-red-600 rounded-md cursor-pointer " onClick={() => setLightboxOpen(true)}>
+                <img src={thumbnail} alt="chapter" className="object-cover w-full h-full rounded-md lg:max-w-full" />
             </div>
             {isLightboxOpen && (
                 <PreviewModal closeLightbox={closeLightbox} content={thumbnail} classNames={`h-full ${item.keyframe_url ? '!w-[55vw] !h-[65vh]' : '!w-1/3 !h-full'}`} />
