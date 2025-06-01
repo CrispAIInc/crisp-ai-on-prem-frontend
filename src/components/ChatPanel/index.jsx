5cb2fdb1d0696c3b08b9db17720121377d34b42c
achraffawzi <achraffawzi224a@gmail.com>
2025-06-01 23:55:56 +0100

    style panels titles

--- src/components/ChatPanel/index.jsx
+++ src/components/ChatPanel/index.jsx
@@ -525,7 +525,7 @@ const ChatPanel = () => {
     >
       <div className={`flex items-center justify-between gap-2 ${theme === "light" ? "text-textColor-300" : "text-textColor-200"
         }`}>
-        <h5 onClick={closeTopTabs} className={` select-none ${theme === "light" ? "text-textColor-100" : "text-textColor-200"
+        <h5 onClick={closeTopTabs} className={`select-none p-[10px]   ${theme === "light" ? "!border-b !border-b-textColor-100/50 text-textColor-100" : "text-textColor-200 !border-b !border-b-textColor-300"
           } cursor-pointer`}>
           Studio</h5>
         {(showEditor || actualTab !== null) && (
--- src/components/ContentPanel/index.jsx
+++ src/components/ContentPanel/index.jsx
@@ -155,7 +155,7 @@ const ContentPanel = () => {
         <aside className={`relative z-10 select-none !h-full content-panel w-1/4 pl-3 bg-background overflow-y-auto overflow-x-hidden ${!isLeftSidebarOpen ? '!w-0 !p-0 !border-none' : "px-2"} flex flex-col relative`} ref={contentPanelContainerRef} style={{
             width: leftWidth
         }}>
-            <h5 className={`select-none ${theme === "light" ? "text-textColor-100" : "text-textColor-200"
+            <h5 className={`select-none p-[10px]   ${theme === "light" ? "!border-b !border-b-textColor-100/50 text-textColor-100" : "text-textColor-200 !border-b !border-b-textColor-300"
                 }`}>Sources</h5>
             {/* <div className="w-56 h-56 bg-blue-500 rounded-full absolute left-90% top-10 -z-1 blur-[160px]"></div> */}
             <div className="w-56 h-56 bg-purple-500 rounded-full absolute left-0 top-40 -z-1 blur-[160px]"></div>
