fc6cabb07b1180e1f59262ae2eaffcbcea60724b
achraffawzi <achraffawzi224a@gmail.com>
2025-06-07 16:06:22 +0100

    display pdf pages and imgs as references in story editor

--- src/components/ChatPanel/index.jsx
+++ src/components/ChatPanel/index.jsx
@@ -1030,12 +1030,12 @@ const ChatPanel = () => {
                                 }
 
                                 {
-                                  content?.pdfsArray?.map((ref, index) => (
+                                  content?.pdfsArr?.map((ref, index) => (
                                     <p onClick={(e) => handlePDFLinkClick(e, ref)} className="mb-2 ml-2 break-words cursor-pointer text-primary-300 w-fit" key={index}>{ref?.source_path} | {ref?.timestamp}</p>
                                   ))
                                 }
                                 {
-                                  content?.imgsArray?.map((ref, index) => (
+                                  content?.imgsArr?.map((ref, index) => (
                                     <p onClick={(e) => handlePDFLinkClick(e, ref)} className="mb-2 ml-2 break-words cursor-pointer text-primary-300 w-fit" key={index}>{ref?.source_path} | {ref?.timestamp}</p>
                                   ))
                                 }
