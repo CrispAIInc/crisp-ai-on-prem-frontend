d3d965674000b593d90f23e949a3664cc8f0a11a
achraffawzi <achraffawzi224a@gmail.com>
2025-06-20 14:56:09 +0100

    call translate-chat when language dropdown changes

--- src/components/CustomSelectTwo/index.jsx
+++ src/components/CustomSelectTwo/index.jsx
@@ -5,17 +5,17 @@ const CustomSelectTwo = ({ options, onChange, placeholder, dropdownText }) => {
 
     const { theme } = useContext(MainContext);
 
-    // const handleChange = (event) => {
-    //     const selectedOption = options.find(
-    //         (option) => option.value === event.target.value
-    //     );
-    //     setSelectedValue(event.target.value);
-    //     onChange(selectedOption);
-    // };
+    const handleChange = (event) => {
+        const selectedOption = options.find(
+            (option) => option.value === event.target.value
+        );
+        // setSelectedValue(event.target.value);
+        onChange(selectedOption);
+    };
 
     return (
         <div className="custom-select-container user-select-none">
-            <select className={`px-1 cursor-pointer py-1 rounded-md outline-none ${theme === 'light' ? 'bg-light-hover-100/30 border border-textColor-100' : 'bg-light-hover-200/20 text-textColor-100  !border !border-textColor-300'}`} value={dropdownText}>
+            <select className={`px-1 cursor-pointer py-1 rounded-md outline-none ${theme === 'light' ? 'bg-light-hover-100/30 border border-textColor-100' : 'bg-light-hover-200/20 text-textColor-100  !border !border-textColor-300'}`} onChange={handleChange} value={dropdownText}>
                 <option value="" disabled className={`${theme === 'dark' && 'bg-background text-textColor-100'}`}>{placeholder}</option>
                 {options.map((option) => (
                     <option key={option.value} value={option.value} className={`${theme === 'dark' && 'bg-background text-textColor-100 border'}`}>
