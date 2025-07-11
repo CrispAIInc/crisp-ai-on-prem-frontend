a8dfd42d8ab198e2f9ef474b00b784054b2449a8
achraffawzi <achraffawzi224a@gmail.com>
2025-07-11 15:25:40 +0100

    add description for reel context

--- src/components/MediaEntertainment/index.jsx
+++ src/components/MediaEntertainment/index.jsx
@@ -99,7 +99,10 @@ function MediaEntertainment({ reel,
         <div className='z-20 flex flex-col gap-3'>
             {/* context */}
             <div className="relative w-full mt-6">
-                <label className={`${theme === "dark" ? 'text-textColor-100' : 'text-textColor-200'} font-medium`}>Your reel topic</label>
+                <div className="flex flex-col mb-2">
+                    <label className={`${theme === "dark" ? 'text-textColor-100' : 'text-textColor-200'} font-medium`}>Your reel topic</label>
+                    <span className={`${theme === "dark" ? 'text-textColor-100' : 'text-textColor-200'} text-sm`}>When no context or topic is provided, the reel will be based on the existing highlights.</span>
+                </div>
                 <textarea
                     className={`w-full p-2 bg-transparent !border ${theme === "dark" ? "!border !border-textColor-200/50 rounded-md text-textColor-200" : '!border !border-textColor-100 text-textColor-300'} rounded-md resize-none focus:outline-none focus:ring-2 focus:ring-blue-500`}
                     rows="3"
