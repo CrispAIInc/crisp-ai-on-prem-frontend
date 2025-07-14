04b93011ed43cff3cefd1de081985f1a22fe4e51
achraffawzi <achraffawzi224a@gmail.com>
2025-07-14 16:31:25 +0100

    set word break all for source names in content section displayedSources state

--- src/components/ContentSection/index.jsx
+++ src/components/ContentSection/index.jsx
@@ -773,7 +773,7 @@ const ContentSection = ({
                                                 <img className="object-cover w-full h-full rounded-md" src={`${API_ENDPOINT}/${option?.file_type === 'video' ? 'thumbnails' : option?.file_type === 'pdf' ? 'pdf-thumbnails' : 'img-thumbnails'}/${encodeURIComponent(option?.category[0])}/${encodeURIComponent(option?.thumbnail)}`}
                                                     alt="Video Thumbnail" />
                                             </div>
-                                            <span className={`text-md font-medium break-words ${theme === 'dark' && 'text-textColor-100'}`}>{option.source_path.replace(/\.[^/.]+$/, '')}</span>
+                                            <span className={`text-md font-medium break-all ${theme === 'dark' && 'text-textColor-100'}`}>{option.source_path.replace(/\.[^/.]+$/, '')}</span>
                                         </div>
                                         <div className="flex items-center ">
                                             <Checkbox
--- src/hooks/useResizableSidebar.js
+++ src/hooks/useResizableSidebar.js
@@ -4,7 +4,7 @@ import useCheckMobileScreen from './useCheckMobileScreen';
 
 export const useResizableSidebar = (minWidth, isLeft) => {
     const isMobile = useCheckMobileScreen();
-    const maxWidth = window.innerWidth * 0.75;
+    const maxWidth = window.innerWidth * 0.65;
     const [sidebarWidth, setSidebarWidth] = useState(isMobile ? -100 : window.innerWidth / 3.3333);
     const [isResizing, setIsResizing] = useState(false);
 
