e0ff79d98213a3de9aeecfd57e2721b9c4883aab
achraffawzi <achraffawzi224a@gmail.com>
2025-07-14 21:38:46 +0100

    make base headings unselectable

--- src/components/BaseHeading/index.jsx
+++ src/components/BaseHeading/index.jsx
@@ -1,6 +1,6 @@
 const BaseHeading = ({ text, className = '', onClick }) => {
     return (
-        <p className={`m-0 text-sm font-semibold text-textColor-200 ${className}`} onClick={onClick}>{text}</p>
+        <p className={`m-0 text-sm font-semibold text-textColor-200 select-none ${className}`} onClick={onClick}>{text}</p>
     );
 };
 
