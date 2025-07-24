df31a2bfea379e223be91cb4b51cc06ee0c597a2
achraffawzi <achraffawzi224a@gmail.com>
2025-07-24 18:04:05 +0100

    add link in sign in page to direct to fprget password view

--- src/components/Auth/Login.jsx
+++ src/components/Auth/Login.jsx
@@ -73,6 +73,7 @@ export default function Register() {
                     <span>Sign in</span>
                 </RippleButton>
             </div>
+            <p className='mt-2 font-medium text-center text-textColor-200'>Forgot password? <Link className="text-primary-300" to="/forgot-password">click here!</Link></p>
             <p className='mt-2 font-medium text-center text-textColor-200'>Don&apos;t  have an account? <Link className="text-primary-300" to="/sign-up">Sign up</Link></p>
         </div>
     );
