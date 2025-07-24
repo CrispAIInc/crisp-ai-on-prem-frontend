d7047cbc9bc625a074b9420e24e12cb67b305b44
achraffawzi <achraffawzi224a@gmail.com>
2025-07-24 17:54:06 +0100

    create reset password component

---
+++ src/components/Auth/ResetPassword.jsx
@@ -0,0 +1,71 @@
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+import AnimatedInput from '../AnimatedInput';
+import RippleButton from "../RippleButton";
+
+export default function ResetPassword() {
+    const [password, setPassword] = useState("");
+    const [confirmPassword, setConfirmPassword] = useState("");
+    const [isPending, setIsPending] = useState(false);
+    const [error, setError] = useState(null);
+
+    function register() {
+        try {
+            setIsPending(true);
+            setError(null);
+
+            // Validate user input
+            if (!password || !confirmPassword) {
+                throw new Error("All fields are required.");
+            }
+            if (password !== confirmPassword) {
+                throw new Error("Passwords do not match.");
+            }
+
+            // Here you would typically make an API call to register the user
+
+            // Reset userInfo after registration attempt
+            setPassword("");
+            setConfirmPassword("");
+        } catch (e) {
+            setError(e.message || "Please verify your data and try again.");
+        } finally {
+            setIsPending(false);
+        }
+    }
+
+    return (
+        <div className="flex flex-col w-full">
+            <img src="./imgs/app-logo-full.png" alt="CrispAI logo" className='w-[50%] h-auto mx-auto mb-10' />
+            <h1 className="mb-10 text-4xl font-bold text-center">Sign-in</h1>
+            {
+                error && <p className="mb-4 text-center text-red-500">{error}</p>
+            }
+            <div className="flex flex-col gap-4">
+                <AnimatedInput
+                    isPassword
+                    name="password"
+                    inputClasses="!pl-[20px]"
+                    label="Password"
+                    value={password}
+                    setValue={(value) => setPassword(value)}
+                    type="password"
+                />
+                <AnimatedInput
+                    isPassword
+                    name="confirmPassword"
+                    inputClasses="!pl-[20px]"
+                    label="Confirm password"
+                    value={confirmPassword}
+                    setValue={(value) => setConfirmPassword(value)}
+                    type="password"
+                />
+                <RippleButton fullWidth cssClasses="flex items-center py-2 pl-2 !pr-3 gap-2" onClick={register}>
+                    {isPending && <span className="loader-atom"></span>}
+                    <span>Submit</span>
+                </RippleButton>
+            </div>
+            <p className='mt-2 font-medium text-center text-textColor-200'>Remembered your password? <Link className="text-primary-300" to="/login">Sign in</Link></p>
+        </div>
+    );
+}
