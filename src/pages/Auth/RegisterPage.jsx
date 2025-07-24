84529dacd5c14d409ed53eea7e5f9501e665d07e
achraffawzi <achraffawzi224a@gmail.com>
2025-07-24 15:27:53 +0100

    handle case when user has account

--- src/pages/Auth/RegisterPage.jsx
+++ src/pages/Auth/RegisterPage.jsx
@@ -4,7 +4,7 @@ import Register from "../../components/Auth/Register";
 
 function RegisterPage({ theme, setTheme }) {
     return (
-        <div className="flex min-h-screen">
+        <div className="flex min-h-screen ">
             {/* left showcase => gradient wave design*/}
             <div className='relative hidden xl:block p-[4px] w-1/2 max-w-1/2 3xl:w-1/3 4xl:w-1/4 h-screen max-h-screen'>
                 <div className='flex items-center text-white absolute top-[5%] left-[5%] z-10'>
@@ -15,8 +15,8 @@ function RegisterPage({ theme, setTheme }) {
                 <h2 className="absolute right-0 z-10 text-white top-[80%] w-2/3">The X factor in Rich Media understanding</h2>
             </div>
             {/* signup form */}
-            <div className="flex items-center justify-center w-full h-screen xl:w-1/2">
-                <div className="w-full max-w-md px-8">
+            <div className="flex items-center justify-center flex-1 w-full h-screen xl:w-1/2">
+                <div className="w-full max-w-md px-8 ">
                     <Register theme={theme} />
                 </div>
             </div>
