743a34e2045a9b32a64dba91276a6f4cf69e8f81
achraffawzi <achraffawzi224a@gmail.com>
2025-07-27 12:05:09 +0100

    add text gradient on hover in source explorer categories and file types

--- src/components/SourceExplorer/index.jsx
+++ src/components/SourceExplorer/index.jsx
@@ -137,7 +137,7 @@ export function SourceExplorer(props) {
         return <>
             {props[viewModes[viewModes.length - 1]].map((item, index) => (
                 <div
-                    className="relative transition-transform folder hover:scale-110 hover:font-medium"
+                    className="relative transition-transform folder group hover:scale-110 hover:font-medium hover:bg-gradient-to-r hover:from-[#755bea] hover:to-[#b76894] hover:bg-clip-text hover:text-transparent"
                     onClick={() => (viewModes[viewModes.length - 1] === "categories" ? openCategoryFolder(item.value) : openFormatFolder(item.value))}
                     key={index}
                     onMouseOver={() => setItemToRemove(item.value)}
@@ -145,8 +145,8 @@ export function SourceExplorer(props) {
                 >
                     {(itemToRemove === item.value && viewModes[viewModes.length - 1] === "categories") && <DeleteIcon color='error' onClick={(e) => removeIndex(e)} className='absolute top-0 right-3' />}
                     {/* <FolderIcon sx={{ fontSize: 60 }} /> */}
-                    <FolderOpenIcon style={{ color: `${theme === 'light' ? '#333' : '#ABAEB4'}` }} sx={{ fontSize: 50 }} />
-                    <p className="">{item.label}</p>
+                    <FolderOpenIcon sx={{ fontSize: 50 }} className={`${theme === 'light' ? 'text-textColor-300' : "text-[#ABAEB4]"} `} />
+                    <p>{item.label}</p>
                 </div>
             ))}
             <RemoveIndexModal deleteResource={props.deleteResource} index={itemToRemove} show={showRemoveIndexModal} onHide={() => setShowRemoveIndexModal(false)} />
