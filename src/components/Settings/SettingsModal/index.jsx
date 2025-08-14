cd05f6c65e75469a106c5304c71f9234abd171e3
achraffawzi <achraffawzi224a@gmail.com>
2025-08-14 11:05:00 +0100

    create account settings

--- src/components/Settings/SettingsModal/index.jsx
+++ src/components/Settings/SettingsModal/index.jsx
@@ -1,11 +1,29 @@
 import { useContext, useState } from 'react';
 import Modal from 'react-bootstrap/Modal';
-import Form from 'react-bootstrap/Form';
-import { MainContext } from '../../contexts/mainContext';
+import { MainContext } from '../../../contexts/mainContext';
+import GeneralSettings from '../GeneralSettings';
+import AccountSettings from "../AccountSettings";
 
 export function SettingsModal(props) {
 
     const { theme } = useContext(MainContext);
+
+    const [activeTab, setActiveTab] = useState("General");
+
+    const renderActiveSettingsTab = () => {
+        switch (activeTab) {
+            case "General":
+                return <GeneralSettings />;
+            case "Account":
+                return <AccountSettings />;
+            // case "Privacy":
+            //     return <div>Privacy settings content goes here...</div>;
+            // case "Notifications":
+            //     return <div>Notifications settings content goes here...</div>;
+            default:
+                return null;
+        }
+    };
 
     return (
         <Modal
@@ -15,24 +33,26 @@ export function SettingsModal(props) {
             aria-labelledby="contained-modal-title-vcenter"
             scrollable={true}
             centered
-            className="note-modal"
+            className="note-modal rounded-3xl"
+            dialogClassName="custom-rounded"
         >
             <Modal.Header closeButton className={`${theme === 'light' ? '' : 'bg-textColor-300 text-white !border-b-textColor-200'}`}>
                 <Modal.Title id="contained-modal-title-vcenter" className="flex items-center justify-between py-3">
-                    <p>Settings</p>
-                    <span>&times;</span>
+                    <h3 className="mb-0 text-xl">Settings</h3>
                 </Modal.Title>
             </Modal.Header>
             <Modal.Body className={`${theme === 'light' ? '' : 'bg-textColor-300 text-white'}`}>
-                <div>
+                <div className="flex items-center gap-3 text-xl">
                     {
-                        ["General", "Account", "Privacy", "Notifications"].map((setting, index) => (
+                        ["General", "Account"].map((setting, index) => (
                             <div key={index} className={`py-2 px-3 rounded-md mb-2 ${theme === 'light' ? 'hover:bg-light-hover-100' : 'hover:bg-background_workspace'}`}>
-                                <p className={`text-sm ${theme === 'light' ? 'text-textColor-300' : 'text-textColor-100'}`}>{setting}</p>
+                                <h3 onClick={() => setActiveTab(setting)} className={`text-xl cursor-pointer hover:font-medium ${theme === 'light' ? 'text-textColor-300' : 'text-textColor-100'} ${setting === activeTab && 'text-gradient-x'}`}>{setting}</h3>
                             </div>
                         ))
                     }
                 </div>
+
+                <div className="mt-4 text-gray-700">{renderActiveSettingsTab()}</div>
             </Modal.Body>
             <Modal.Footer className={`${theme === "light" ? "" : "!bg-textColor-300 !text-white !border-t !border-t-textColor-200"}`}>
                 <div
