78931bf5df49754603e38542285b3cde6e246940
achraffawzi <achraffawzi224a@gmail.com>
2025-08-20 16:54:20 +0100

    connect Firebase config to backend

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -12,13 +12,13 @@ import { getAuth } from "firebase/auth";
 // export const auth = getAuth(app);
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDe2BEaMyhQTugA9ZdWC5MlJQCqtdOVjXo",
-    authDomain: "crispai-app-462614.firebaseapp.com",
-    projectId: "crispai-app-462614",
-    storageBucket: "crispai-app-462614.firebasestorage.app",
-    messagingSenderId: "843040371870",
-    appId: "1:843040371870:web:ad761e88bdca291908f78f",
-    measurementId: "G-D6J7KPEZK1"
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_FIREBASE_APP_ID,
+    measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
