42ced88d9572e2ce0b07905640b9d432851ac980
achraffawzi <achraffawzi224a@gmail.com>
2025-08-30 11:55:45 +0100

    send sources categories except "all" category in /gen-metadata

--- src/components/MetadataGen/index.jsx
+++ src/components/MetadataGen/index.jsx
@@ -74,12 +74,26 @@ function MetadataGen() {
 
         const categoryValues = categoryOptions.map((option) => option.value);
 
+        function getCategories(items) {
+            const categories = new Set();
+
+            items.forEach(item => {
+                item.category.forEach(cat => {
+                    if (cat.toLowerCase() !== "all") {
+                        categories.add(cat);
+                    }
+                });
+            });
+
+            return Array.from(categories);
+        }
+
         try {
             // const payload = {
             //     category: selectedCategory, sources: knowledgeBase.filter(kb => kb.is_selected).map(kb => ({ file_type: kb.file_type, source_path: kb.source_path })), selectedOptions: selectedOptions.map(op => op.id), verbosityValue, temperatureValue
             // };
             const payload = {
-                category: selectedCategory, sources: displayedSources.filter(item => item.is_selected).map(source => ({ file_type: source.file_type, source_path: source.source_path })), selectedOptions: selectedOptions.map(op => op.id), inputContext: context, verbosityValue: verbosityValue
+                category: getCategories(displayedSources.filter(item => item.is_selected)), sources: displayedSources.filter(item => item.is_selected).map(source => ({ file_type: source.file_type, source_path: source.source_path })), selectedOptions: selectedOptions.map(op => op.id), inputContext: context, verbosityValue: verbosityValue
             };
             setSourcesTobeCommited(knowledgeBase.filter(kb => kb.is_selected));
             // setSourcesAfterUncheckCrispWiz(sourcesTobeCommited);
