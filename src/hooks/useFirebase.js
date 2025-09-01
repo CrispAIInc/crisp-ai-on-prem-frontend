d43beae68395e4c802101400a8b865b7495d950e
achraffawzi <achraffawzi224a@gmail.com>
2025-09-02 03:03:13 +0700

    fix firebase config import path

--- src/hooks/useFirebase.js
+++ src/hooks/useFirebase.js
@@ -1,5 +1,5 @@
 import { ref, getDownloadURL } from "firebase/storage";
-import { storage } from "./firebase";
+import { storage } from "../config/firebase";
 
 export default function useFirebase() {
 
