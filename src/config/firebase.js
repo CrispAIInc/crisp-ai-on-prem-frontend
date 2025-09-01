cbdfb3c24f2bf1f6de68c263af87e5c491095498
achraffawzi <achraffawzi224a@gmail.com>
2025-09-02 02:00:04 +0700

    import storage service from Firebase

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,6 +1,7 @@
 // firebase.js
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getStorage } from "firebase/storage";
 
 // const firebaseConfig = {
 //     apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
@@ -24,4 +25,5 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
+export const storage = getStorage(app);
 // const analytics = getAnalytics(app);
