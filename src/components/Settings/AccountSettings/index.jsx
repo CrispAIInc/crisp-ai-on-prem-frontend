99b3f8bd204d70efc2630664c48751fc1eead8ad
achraffawzi <achraffawzi224a@gmail.com>
2025-09-03 23:53:32 +0700

    add theme toggle switch in general settings tab

--- src/components/Settings/AccountSettings/index.jsx
+++ src/components/Settings/AccountSettings/index.jsx
@@ -2,7 +2,10 @@ import React from 'react';
 
 function AccountSettings() {
     return (
-        <div>AccountSettings</div>
+        <div className="flex flex-col gap-5">
+            {/* app theme switcher */}
+            <p>account settings</p>
+        </div>
     );
 }
 
--- src/components/Settings/GeneralSettings/index.jsx
+++ src/components/Settings/GeneralSettings/index.jsx
@@ -1,8 +1,21 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import ThemeToggle from '../../ThemeToggle';
+import { MainContext } from '../../../contexts/mainContext';
 
 function GeneralSettings() {
+    const { theme } = useContext(MainContext);
+
     return (
-        <div>GeneralSettings</div>
+        <div className="flex flex-col gap-5">
+            {/* app theme switcher */}
+            <div className="flex items-center justify-between">
+                <h3 className={`text-[15px] ${theme === "light"
+                    ? "text-textColor-300"
+                    : "text-textColor-100"
+                    }`}>Theme</h3>
+                <ThemeToggle />
+            </div>
+        </div>
     );
 }
 
