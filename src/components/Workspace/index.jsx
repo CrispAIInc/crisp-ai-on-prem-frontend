9f04a68e9d1096b1341095b68e0303a276fb3da7
achraffawzi <achraffawzi224a@gmail.com>
2025-09-03 15:23:04 +0700

    add combined summary loading flag to center panel component props

--- src/components/Workspace/index.jsx
+++ src/components/Workspace/index.jsx
@@ -51,7 +51,7 @@ const Workspace = () => {
                 <SwapHorizOutlinedIcon className={`cursor-pointer ${theme === 'dark' && 'text-textColor-100'}`} onClick={() => setIsLeftSidebarOpen(!isLeftSidebarOpen)} />
             </div>
             {(activeView === 'resource' || displayedSources?.length > 0) ? (
-                <CenterPanel combinedSummary={combinedSummary} setCombinedSummary={setCombinedSummary} />
+                <CenterPanel combinedSummary={combinedSummary} setCombinedSummary={setCombinedSummary} isCombinedSummaryPending={isCombinedSummaryPending} setIsCombinedSummaryPending={setIsCombinedSummaryPending} />
             ) : !activeView ? (
                 <div className="mt-10">
                     <NoData />
