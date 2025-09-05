5cb369be460429c020c2fc4a8c3521bc9eaad396
achraffawzi <achraffawzi224a@gmail.com>
2025-09-06 00:29:35 +0700

    create app settings context api

---
+++ src/contexts/settingsContext.jsx
@@ -0,0 +1,16 @@
+import { createContext, useState } from "react";
+
+export const SettingsContext = createContext();
+
+export default function SettingsProvider({ children }) {
+    const [generalSettings, setGeneralSettings] = useState({
+        video_autoplay: false,
+        video_loop: false,
+    });
+
+    return (
+        <SettingsContext.Provider value={{ generalSettings, setGeneralSettings }}>
+            {children}
+        </SettingsContext.Provider>
+    );
+}
