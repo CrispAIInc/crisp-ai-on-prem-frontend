6280f792e31c6ed8ed629f067062a1073e3f0f8d
achraffawzi <achraffawzi224a@gmail.com>
2025-09-16 16:29:50 +0700

    configure firebase google auth

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,6 +1,6 @@
 // firebase.js
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
+import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 import { getStorage } from "firebase/storage";
 
 // const firebaseConfig = {
@@ -26,4 +26,5 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const storage = getStorage(app);
+export const provider = new GoogleAuthProvider();
 // const analytics = getAnalytics(app);
