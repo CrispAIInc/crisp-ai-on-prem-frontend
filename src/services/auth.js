fc6880aeae9f356ee5cd4674ae6f4722045b17e9
achraffawzi <achraffawzi224a@gmail.com>
2025-09-19 03:04:57 +0700

    move custom login functionality to auth service

--- src/services/auth.js
+++ src/services/auth.js
@@ -1,6 +1,7 @@
-import { signInWithPopup } from 'firebase/auth';
+import { signInWithCustomToken, signInWithPopup } from 'firebase/auth';
 import { TOKEN_NAME } from '../globals';
 import { auth, provider } from '../config/firebase';
+import makeApiRequest from '../api';
 
 
 export const signInWithGoogle = async () => {
@@ -11,5 +12,25 @@ export const signInWithGoogle = async () => {
         localStorage.setItem(TOKEN_NAME, token);
     } catch (err) {
         console.error(err);
+    }
+};
+
+export const signInWithEmailAndPassword = async (userInfo) => {
+    const { success, accessToken, message } = await makeApiRequest('/login', 'POST', JSON.stringify(userInfo));
+
+    if (success) {
+        // Step 3: Exchange custom token for Firebase ID token
+        const userCredential = await signInWithCustomToken(auth, accessToken);
+
+        console.log("User signed in:", userCredential.user);
+
+        // Now you can use getIdToken() anytime
+        const idToken = await userCredential.user.getIdToken();
+        console.log("Firebase ID Token:", idToken);
+
+        // Optionally store idToken if you want
+        localStorage.setItem(TOKEN_NAME, idToken);
+    } else {
+        throw new Error(message || "Login failed. Please try again.");
     }
 };
