4bc3434347a32eb060f8e9b102b86c8b07acea6f
achraffawzi <achraffawzi224a@gmail.com>
2025-10-11 13:27:12 +0700

    hide upload when no index exist in DB

--- src/components/AddSourceModal/index.jsx
+++ src/components/AddSourceModal/index.jsx
@@ -52,13 +52,13 @@ export default function LLMModal(props) {
                     <span className={`font-medium ${theme === 'light' ? 'text-textColor-300' : 'text-textColor-100'}`}>New Index</span>
 
                 </div>
-                <div
+                {categoryOptions?.filter(cat => cat.value !== "all").length > 0 && <div
                     className={`flex items-center justify-center gap-2 px-2 py-2 rounded-md cursor-pointer w-fit ${theme === 'light' ? 'hover:bg-light-hover-100/30' : 'hover:bg-light-hover-200/20'}`}
                     onClick={handleAddNewResource}
                 >
                     {props?.isUploading ? (<LoadingSpinner isSmall />) : (<UploadIcon style={{ color: `${theme === 'light' ? '#333' : '#ABAEB4'}` }} />)}
                     <span className={`font-medium ${theme === 'light' ? 'text-textColor-300' : 'text-textColor-100'}`}>Upload</span>
-                </div>
+                </div>}
                 <IndexModal show={isIndexModalOpen} onHide={hideIndexModal} handleUpload={props?.handleUpload} />
                 <CategoriesModal
                     show={showCategoriesModal}
