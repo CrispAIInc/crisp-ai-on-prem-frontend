e315a659d7fa84b3043ffcebad0f6dec367a3acd
achraffawzi <achraffawzi224a@gmail.com>
2025-10-15 11:59:01 +0700

    replace reel_video_path with reel_video_url for viewing reels

--- src/components/ReelViewer/index.jsx
+++ src/components/ReelViewer/index.jsx
@@ -28,12 +28,12 @@ function ReelViewer({ closeReel, reel, setReels }) {
     // const [isPending, setIsPending] = useState(false);
 
     useEffect(() => {
-        if (reel?.reel_video_url || reel?.reel_video_path) {
-            getPublicUrl(reel?.reel_video_url || reel?.reel_video_path)
+        if (reel?.reel_video_url) {
+            getPublicUrl(reel?.reel_video_url)
                 .then(setSourcePublicUrl)
                 .catch(console.error);
         }
-    }, [reel?.reel_video_url || reel?.reel_video_path]);
+    }, [reel?.reel_video_url]);
 
     const handleCloseReel = (e) => {
         e.stopPropagation();
