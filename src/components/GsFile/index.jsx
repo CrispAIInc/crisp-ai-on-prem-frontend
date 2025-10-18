d7600b274d58241c11525326e60a33a4d244b32c
achraffawzi <achraffawzi224a@gmail.com>
2025-10-19 00:14:21 +0700

    prevent converting blob link to public url

--- src/components/GsFile/index.jsx
+++ src/components/GsFile/index.jsx
@@ -8,6 +8,10 @@ export default function GsFile({ gsUrl, type = "img", alt = "", ...props }) {
 
     useEffect(() => {
         if (!gsUrl) return;
+        if (gsUrl.startsWith('blob')) {
+            setUrl(gsUrl);
+            return;
+        }
         getPublicUrl(gsUrl).then(setUrl).catch(console.error);
     }, [gsUrl]);
 
