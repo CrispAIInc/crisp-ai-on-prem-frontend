c7d318ac3de694b336b20af32a4bee6ebb85cd47
achraffawzi <achraffawzi224a@gmail.com>
2025-10-23 17:52:30 +0700

    add text color to animated text for dark mode

--- src/components/AnimatedText/index.jsx
+++ src/components/AnimatedText/index.jsx
@@ -1,10 +1,15 @@
+import { useContext } from 'react';
+import { MainContext } from '../../contexts/mainContext';
+
 export default function AnimatedText({ text = 'Processing', cssClasses = "" }) {
+    const { theme } = useContext(MainContext);
     return (
         <span className={`inline-block`}>
             {text.split('').map((char, index) => (
                 <span
                     key={index}
-                    className={`inline-block text-xs animate-fade-in ${cssClasses}`}
+                    className={`inline-block text-xs animate-fade-in ${theme === "light" ? "text-textColor-300" : "text-textColor-100"
+                        } ${cssClasses}`}
                     style={{ animationDelay: `${index * 0.01}s` }}
                 >
                     {char === ' ' ? '\u00A0' : char}
