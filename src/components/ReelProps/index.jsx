85f0ac922104bcf12e684b8c73b43e22df59bc81
achraffawzi <achraffawzi224a@gmail.com>
2025-10-26 21:17:18 +0700

    remove margin from 'reel properties'  title

--- src/components/ReelProps/index.jsx
+++ src/components/ReelProps/index.jsx
@@ -38,7 +38,7 @@ function ReelProps({ reel = {
     return (
         <div className={`p-4 w-[30vw] ${theme === 'light' ? "text-textColor-300 bg-[#f0f0f0]" : "text-textColor-100 bg-textColor-300"} flex-1 flex flex-col gap-3 max-h-full overflow-y-hidden`}>
             <div className="flex items-center justify-between">
-                <h5 className='text-gradient-x'>Reel Properties</h5>
+                <h5 className='mb-0 text-gradient-x'>Reel Properties</h5>
                 <KeyboardReturnIcon style={{ color: `${theme === 'light' ? '#333' : '#ABAEB4'}` }} className="rotate-180 cursor-pointer" onClick={closeReelProps} />
             </div>
             <div className='flex flex-col flex-1 overflow-y-hidden'>
