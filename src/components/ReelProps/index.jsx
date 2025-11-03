0bb99a98713afc0c14a75db92aef4c44da63a9c0
achraffawzi <achraffawzi224a@gmail.com>
2025-11-03 12:24:34 +0800

    break words for reel segment properties

--- src/components/ReelProps/index.jsx
+++ src/components/ReelProps/index.jsx
@@ -78,16 +78,16 @@ function ReelProps({ reel, closeReelProps = () => { } }) {
                                         <div className="flex items-center gap-1">
                                             <div className="flex items-center gap-1">
                                                 <TitleIcon fontSize='small' style={{ color: `${theme === 'light' ? '#333' : '#ABAEB4'}` }} />
-                                                <h6 className="mb-0 text-sm">{segment?.title}</h6>
+                                                <h6 className="mb-0 text-sm break-all">{segment?.title}</h6>
                                             </div>
                                         </div>
                                         <div className="flex items-center gap-1">
                                             <AccessTimeIcon fontSize='small' style={{ color: `${theme === 'light' ? '#333' : '#ABAEB4'}` }} />
-                                            <h6 className="mb-0 text-sm">{formatDuration(segment?.duration)}</h6>
+                                            <h6 className="mb-0 text-sm break-all">{formatDuration(segment?.duration)}</h6>
                                         </div>
                                         <div className="flex items-center gap-1">
                                             <PlayCircleOutlineIcon fontSize='small' style={{ color: `${theme === 'light' ? '#333' : '#ABAEB4'}` }} />
-                                            <h6 className="mb-0 text-sm truncate">{segment?.source_filename}</h6>
+                                            <h6 className="mb-0 text-sm break-all">{segment?.source_filename}</h6>
                                         </div>
                                     </div>
                                     <hr className="my-0 border-gray-300"></hr>
