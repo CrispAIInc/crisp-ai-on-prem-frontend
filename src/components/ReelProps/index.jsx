98883fe5842e90c5d68496b2d81ecc0428edc37b
achraffawzi <achraffawzi224a@gmail.com>
2025-11-03 15:29:19 +0800

    exclude combined_video_info and original_sources from reel props

--- src/components/ReelProps/index.jsx
+++ src/components/ReelProps/index.jsx
@@ -21,7 +21,7 @@ import InfoOutlinedIcon from '@mui/icons-material/InfoOutlined';
 import GsFile from '../GsFile';
 
 function ReelProps({ reel, closeReelProps = () => { } }) {
-    const { filename, id, reel_video_url, thumbnail, user_id, ...rest } = reel;
+    const { filename, id, reel_video_url, thumbnail, user_id, combined_video_info, original_sources, ...rest } = reel;
     const { theme } = useContext(MainContext);
     return (
         <div className={`p-4 w-[30vw] ${theme === 'light' ? "text-textColor-300 bg-[#f0f0f0]" : "text-textColor-100 bg-textColor-300"} flex-1 flex flex-col gap-3`}>
