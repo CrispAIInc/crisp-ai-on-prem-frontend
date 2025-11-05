ffbb2e2c20d72562d2f3be9b0fef15b901135fb5
achraffawzi <achraffawzi224a@gmail.com>
2025-11-05 22:53:55 +0800

    set gap between discovery result sources to 1

--- src/components/SearchModal/index.jsx
+++ src/components/SearchModal/index.jsx
@@ -46,7 +46,7 @@ export function SearchModal(props) {
                     </Modal.Title>
                 </Modal.Header>
                 <Modal.Body className={`${theme === "light" ? "" : "bg-textColor-300 text-white"}`}>
-                    <div className="flex flex-col gap-3">
+                    <div className="flex flex-col gap-1">
                         {filteredKnowledgeBase.map((item, index) => (
                             // <div className={`!border ${theme === 'dark' && '!border-textColor-200'} !h-fit w-[130px] relative cursor-pointer`} key={index}>
                             // {props.isDeleting && props.clickedIndex === index ? (
