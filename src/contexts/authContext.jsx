6161f4d28cb46d7ce846f0332e8c8a057a484849
achraffawzi <achraffawzi224a@gmail.com>
2025-11-12 19:26:13 +0800

    include username in general settings view

--- src/contexts/authContext.jsx
+++ src/contexts/authContext.jsx
@@ -4,9 +4,9 @@ export const AuthContext = createContext();
 
 export default function AuthProvider({ children }) {
     const [user, setUser] = useState({
+        username: "",
         firstName: "",
         lastName: "",
-        username: "",
         email: "",
         emailVerified: false,
     });
