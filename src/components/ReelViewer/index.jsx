538c0d71430a62988416db7ea44faa52cce126f0
achraffawzi <achraffawzi224a@gmail.com>
2025-12-01 22:26:57 +0800

    prevent reel full size dragging

--- src/components/ReelViewer/index.jsx
+++ src/components/ReelViewer/index.jsx
@@ -261,7 +261,7 @@ function ReelViewer({
                     <div className="w-56 h-56 bg-purple-500 rounded-full absolute left-35 top-[50%] z-10 blur-[160px]"></div>
                     <div className="w-56 h-56 bg-pink-400 rounded-full absolute left-1/2 top-[100%] z-10 blur-[160px]"></div>
 
-                    <div className="absolute left-0 flex items-center justify-between w-full gap-3 p-1 top-8">
+                    <div className="absolute left-0 flex items-center justify-between w-full gap-3 p-1 top-3">
                         {areReelControlsVisible && <SwitchTransition mode="out-in">
                             <CSSTransition
                                 key={currentTitle + '-key'}
@@ -328,7 +328,7 @@ function ReelViewer({
                 edge={true}
 
                 /* draggable */
-                draggable={true}
+                draggable={!areReelControlsVisible}
                 throttleDrag={0}
                 onDrag={({
                     target,
