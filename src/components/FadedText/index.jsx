fb76ae4a8fea994c693a6874cab6aae26c527236
achraffawzi <achraffawzi224a@gmail.com>
2025-12-12 22:39:48 +0800

    fix color shade and padding of FadedText component

--- src/components/FadedText/index.jsx
+++ src/components/FadedText/index.jsx
@@ -1,13 +1,15 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { MainContext } from '../../contexts/mainContext';
 
 export default function FadedText({ text }) {
+    const { theme } = useContext(MainContext);
     return (
-        <div className="relative max-w-[220px] overflow-hidden whitespace-nowrap text-gray-300 text-[20px]">
+        <div className={`relative max-w-[220px] overflow-hidden whitespace-nowrap p-1 text-[13px] ${theme === "white" ? "text-textColor-300" : "text-textColor-100"}`}>
             {text}
 
             {/* Right fade shadow */}
-            <div className="pointer-events-none absolute right-0 top-0 h-full w-10
-                      bg-gradient-to-r from-transparent to-[#000000]" />
+            <div className={`pointer-events-none absolute right-0 top-0 h-full w-10
+                      bg-gradient-to-r from-transparent to-background_workspace`} />
         </div>
     );
 }
