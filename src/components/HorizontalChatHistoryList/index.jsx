7c19c81373ff574cb62f9cf5fa250229730a6fa1
achraffawzi <achraffawzi224a@gmail.com>
2025-12-12 22:10:40 +0800

    create HorizontalChatHistoryList  component to horizontally display chat sessions with action buttons

---
+++ src/components/HorizontalChatHistoryList/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function HorizontalChatHistoryList() {
+    return (
+        <div>
+            {/* horizontally scrollable list of chat sessions */}
+
+            {/* icon to add new chat */}
+
+            {/* icon to show all chat sessions grouped by date */}
+        </div>
+    );
+}
+
+export default HorizontalChatHistoryList;
