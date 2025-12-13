ac7e118faf89f1659324c37a907e6388498a8df5
achraffawzi <achraffawzi224a@gmail.com>
2025-12-13 16:44:46 +0800

    set the FadedText minimum height to fit its content

--- src/components/FadedText/index.jsx
+++ src/components/FadedText/index.jsx
@@ -4,11 +4,11 @@ import { MainContext } from '../../contexts/mainContext';
 export default function FadedText({ text }) {
     const { theme } = useContext(MainContext);
     return (
-        <div className={`relative max-w-[220px] overflow-hidden whitespace-nowrap p-1 text-[13px] ${theme === "white" ? "text-textColor-300" : "text-textColor-100"}`}>
+        <div className={`relative min-w-fit max-w-[220px] overflow-hidden whitespace-nowrap p-1 text-[13px] ${theme === "light" ? "text-textColor-300" : "text-textColor-100"}`}>
             {text}
 
             {/* Right fade shadow */}
-            <div className={`pointer-events-none absolute right-0 top-0 h-full w-10
+            <div className={`pointer-events-none absolute right-0 top-0 h-full w-16
                       bg-gradient-to-r from-transparent to-background_workspace`} />
         </div>
     );
