50191986d3fc5e906b7ddc5c812d14e063635b1c
achraffawzi <achraffawzi224a@gmail.com>
2025-12-14 00:03:33 +0800

    add onclick handler to FadedText component

--- src/components/FadedText/index.jsx
+++ src/components/FadedText/index.jsx
@@ -1,10 +1,10 @@
 import React, { useContext } from 'react';
 import { MainContext } from '../../contexts/mainContext';
 
-export default function FadedText({ text }) {
+export default function FadedText({ text, handleClick = () => { } }) {
     const { theme } = useContext(MainContext);
     return (
-        <div className={`relative cursor-pointer min-w-fit max-w-fit overflow-hidden whitespace-nowrap p-1 text-[13px] ${theme === "light" ? "text-textColor-300" : "text-textColor-100 hover:bg-background_workspace"} rounded-full`}>
+        <div onClick={handleClick} className={`relative cursor-pointer min-w-fit max-w-fit overflow-hidden whitespace-nowrap p-1 text-[13px] ${theme === "light" ? "text-textColor-300" : "text-textColor-100 hover:bg-background_workspace"} rounded-full`}>
             {text}
 
             {/* Right fade shadow */}
