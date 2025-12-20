bdb69db079e93ba051b3633188b30524bf9bb087
achraffawzi <achraffawzi224a@gmail.com>
2025-12-21 00:01:48 +0800

    create overlay on project card

--- src/components/ProjectCard/index.jsx
+++ src/components/ProjectCard/index.jsx
@@ -1,8 +1,30 @@
-import React from 'react'
+import React from 'react';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
+import { formatReadableDate } from '../../utils';
 
 const ProjectCard = ({project}) => {
   return (
-    <div>ProjectCard</div>
+    <div style={{background: project.thumbnail ? `url('${project.thumbnail}')` : '#1E1E1E'}} className={`relative  border rounded-2xl p-3 w-80 h-48 ${project.thumbnail ? `!bg-cover !bg-center` : 'bg-[#1E1E1E]'}   shadow-lg transition-shadow duration-300`}>
+        {/* top to bottom gradient overlay */}
+        <div className="absolute inset-0 bg-gradient-to-b from-transparent to-black rounded-2xl pointer-events-none"></div>
+
+        <div className="relative z-50 flex flex-col justify-between h-full  ">
+            {/* top showcase */}
+            <div className="flex justify-between items-center ">
+                <MoreVertIcon className="text-white" />
+                <ArrowForwardIosIcon  className="text-white" />
+            </div>
+            {/* bottom showcase */}
+            <div className="font-semibold">
+                <h2 className="text-2xl font-semibold mb-2 !text-white line-clamp-2">{project.title}</h2>
+                <div className="flex items-center gap-1 flex-wrap">
+                    <p className="text-[10px] text-white">{formatReadableDate(project.created_at)} ~ </p>
+                    <p className="text-[10px] text-white">{project.selectedSources} Sources.</p>
+                </div>
+            </div>
+        </div>
+    </div>
   )
 }
 
--- src/pages/ProjectsPage.jsx
+++ src/pages/ProjectsPage.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import ProjectCard from '../components/ProjectCard';
 
 const ProjectsPage = () => {
+    const projects = [
+        {
+    id: "sfkjsdf",
+    title: "project title",
+    selectedSources: 3,
+    created_at: new Date(),
+    thumbnail: "https://img.lemde.fr/2025/10/28/0/0/4106/2737/664/0/75/0/e2426fc_ftp-import-images-1-mndx2iniwwkc-2025-10-28t050144z-141823617-rc2gyga040c0-rtrmadp-3-climate-change-cop30-gates.JPG"
+}
+    ]
   return (
-    <div>ProjectsPage</div>
+    <div className="p-4">
+        {
+            projects.map((project) => (
+                <ProjectCard key={project.id} project={project} />
+            ))
+        }
+    </div>
   )
 }
 
