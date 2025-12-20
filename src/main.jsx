2cee311872d533fba1de39762fea5d3146f05eaa
achraffawzi <achraffawzi224a@gmail.com>
2025-12-20 21:11:19 +0800

    register store in app

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,9 @@ import './main.css';
 import App from './App';
 import { pdfjs } from 'react-pdf';
 
+import { Provider } from "react-redux";
+import store from "./store";
+
 pdfjs.GlobalWorkerOptions.workerSrc = new URL(
   'pdfjs-dist/build/pdf.worker.min.js',
   import.meta.url,
@@ -12,6 +15,8 @@ pdfjs.GlobalWorkerOptions.workerSrc = new URL(
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   // <React.StrictMode>
-  <App />
+  <Provider store={store}>
+    <App />
+  </Provider>
   // </React.StrictMode>
 );
