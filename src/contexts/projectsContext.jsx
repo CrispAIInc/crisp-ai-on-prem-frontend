8fcb750bd7e47af4ed29eb77328920f241f9f0b4
achraffawzi <achraffawzi224a@gmail.com>
2025-12-22 00:28:41 +0800

    create projects context

---
+++ src/contexts/projectsContext.jsx
@@ -0,0 +1,22 @@
+import { createContext, useEffect, useState } from 'react';
+import makeApiRequest from '../api';
+
+const ProjectsContext = createContext({});
+
+export default function ProjectsProvider({ children, value }) {
+
+    const [projects, setProjects] = useState([]);
+
+    useEffect(() => {
+        async function fetchProjects() {
+            const projects = await makeApiRequest('/projects')
+            console.log(projects);
+        }
+    }, [])
+
+  return (
+    <ProjectsContext.Provider value={value}>
+      {children}
+    </ProjectsContext.Provider>
+  );
+}
