976dd17992978ff367b5ea9ed4fa75cefffca030
achraffawzi <achraffawzi224a@gmail.com>
2025-12-24 02:23:49 +0800

    add setCurrentProject prop to MainWorkspace

--- src/pages/MainWorkspacePage.jsx
+++ src/pages/MainWorkspacePage.jsx
@@ -91,7 +91,7 @@ export default function MainWorkspacePage() {
             </Helmet>
             <div className="!h-full">
                 {/* <MainWorkspace /> */}
-                {currentProject ? <MainWorkspace currentProject={currentProject} /> : <ProjectsPage setCurrentProject={setCurrentProject} projects={projects} setProjects={setProjects} />}
+                {currentProject ? <MainWorkspace currentProject={currentProject} setCurrentProject={setCurrentProject} /> : <ProjectsPage setCurrentProject={setCurrentProject} projects={projects} setProjects={setProjects} />}
             </div>
         </>
     );
