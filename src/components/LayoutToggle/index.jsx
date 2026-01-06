6884e912d9a31a5fc247a75c7fabe554f2ed1e95
achraffawzi <achraffawzi224a@gmail.com>
2026-01-07 00:05:58 +0800

    add plain icons to layout toggler

--- src/components/LayoutToggle/index.jsx
+++ src/components/LayoutToggle/index.jsx
@@ -1,10 +1,12 @@
 import WindowOutlinedIcon from '@mui/icons-material/WindowOutlined';
+import FormatListBulletedOutlinedIcon from '@mui/icons-material/FormatListBulletedOutlined';
 
 export default function LayoutToggle() {
 
     return (
-        <div>
-
+        <div className="flex items-center">
+            <WindowOutlinedIcon />
+            <FormatListBulletedOutlinedIcon />
         </div>
     );
 }
