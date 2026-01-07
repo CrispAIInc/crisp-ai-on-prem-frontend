e42982ba6383654d94553a0863145e99a76715c2
achraffawzi <achraffawzi224a@gmail.com>
2026-01-07 22:38:29 +0800

    prevent duplicate key indexes

--- src/components/MetadataPanel/index.jsx
+++ src/components/MetadataPanel/index.jsx
@@ -447,7 +447,7 @@ const MetadataPanel = ({ workspaceContainer, leftWidth, maxWidth }) => {
                         <div className="flex flex-col gap-3">
                           {
                             Array.isArray(translatedResource?.transcription?.content) && translatedResource?.transcription?.content?.map((topic, index) => (
-                              <div key={index} className="flex flex-col">
+                              <div key={topic.content} className="flex flex-col">
                                 <div>
                                   {/* <h4 className='text-[16px] font-semibold '>{topic.speaker?.toLowerCase()}: </h4> */}
                                   <div className="flex items-center gap-2 cursor-pointer" onClick={() => {
--- src/components/Skeletons/MetadataSkeleton/index.jsx
+++ src/components/Skeletons/MetadataSkeleton/index.jsx
@@ -1,3 +1,4 @@
+import { generateRandomId } from '../../../utils';
 import HeadingSkeleton from '../Base/HeadingSkeleton';
 import TextSkeleton from '../Base/TextSkeleton';
 
@@ -5,16 +6,19 @@ function MetadataSkeleton({ className, numOfLines = 10, numOfOccurences = 3, sho
     return (
         <div className={`flex flex-col gap-4 animate-pulse ${className}`}>
             {
-                new Array(numOfOccurences).fill(null).map(item => (
-                    <div key={item}>
-                        {showHeading && <HeadingSkeleton className='!w-36 h-6 mb-3' />}
-                        {
-                            new Array(numOfLines).fill(null).map((_, index) => (
-                                <TextSkeleton key={index} className='h-3 mb-2' />
-                            ))
-                        }
-                    </div>
-                ))
+                new Array(numOfOccurences).fill(null).map(item => {
+                    let random = generateRandomId(4);
+                    return (
+                        <div key={random}>
+                            {showHeading && <HeadingSkeleton className='!w-36 h-6 mb-3' />}
+                            {
+                                new Array(numOfLines).fill(null).map((_, index) => (
+                                    <TextSkeleton key={index} className='h-3 mb-2' />
+                                ))
+                            }
+                        </div>
+                    );
+                })
             }
         </div>
     );
