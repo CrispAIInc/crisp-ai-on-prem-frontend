bc7ed620e6513dd9b3929cf1e3c69c016624b08c
achraffawzi <achraffawzi224a@gmail.com>
2026-01-13 14:53:33 +0700

    remove container around  toast element inside context

--- src/contexts/toastContext.jsx
+++ src/contexts/toastContext.jsx
@@ -1,7 +1,7 @@
 import { createContext, useContext, useState } from "react";
 import AppAlert from '../components/AppAlert';
 
-const ToastContext = createContext({});
+export const ToastContext = createContext({});
 
 export function ToastProvider({ children }) {
     const [toasts, setToasts] = useState([]);
@@ -23,16 +23,13 @@ export function ToastProvider({ children }) {
         <ToastContext.Provider value={{ notify }}>
             {children}
 
-            {/* Toast container */}
-            <div className="fixed top-6 right-6 z-50 flex flex-col gap-3">
-                {toasts.map((toast) => (
-                    <AppAlert
-                        key={toast.id}
-                        {...toast}
-                        onClose={() => remove(toast.id)}
-                    />
-                ))}
-            </div>
+            {toasts.map((toast) => (
+                <AppAlert
+                    key={toast.id}
+                    {...toast}
+                    onClose={() => remove(toast.id)}
+                />
+            ))}
         </ToastContext.Provider>
     );
 }
