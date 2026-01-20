cc5e2fdcb83054b11e23a6580db7969b1a682487
achraffawzi <achraffawzi224a@gmail.com>
2026-01-20 20:36:43 +0700

    toggle crisp wiz dropdown menu

--- src/components/CrispWizModels/index.jsx
+++ src/components/CrispWizModels/index.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext, useRef, useState } from 'react';
 import TuneIcon from '@mui/icons-material/Tune';
 import { MainContext } from '../../contexts/mainContext';
 
@@ -10,22 +10,37 @@ const CrispWizModels = ({
 
     const { theme } = useContext(MainContext);
 
+    const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+    const modelsDropdownRef = useRef(null);
+
+    function toggleDropdown() {
+        setIsDropdownOpen(!isDropdownOpen);
+    }
+
+    function handleModelChange(modelValue) {
+        setSelectedModel(modelValue);
+        setIsDropdownOpen(false);
+    }
+
+
     return (
         <div className="relative">
             {/* Icon to toggle models dropdown */}
-            <div className={`flex flex-col items-center cursor-pointer justify-center transition rounded-full h-10 w-10
+            <div onClick={toggleDropdown} className={`flex flex-col items-center cursor-pointer justify-center transition rounded-full h-10 w-10
 ${theme === 'light' ? 'hover:bg-textColor-100/20' : 'hover:bg-textColor-300/80'}`}>
                 <TuneIcon className="cursor-pointer" />
             </div>
 
             {/* crisp wiz models dropdown */}
-            <div className={`absolute left-0 z-50 flex flex-col mt-2 overflow-x-hidden shadow-md w-fit rounded-2xl top-7 ${theme === "light" ? "border" : "!border !border-textColor-300"}`}>
-                {
-                    models.map((model) => (
-                        <div
-                            key={model.value}
-                            onClick={() => setSelectedModel(model.value)}
-                            className={`
+            {
+                isDropdownOpen && (
+                    <div ref={modelsDropdownRef} className={`absolute left-0 z-50 flex flex-col mt-2 overflow-x-hidden shadow-md w-fit rounded-2xl top-7 ${theme === "light" ? "border" : "!border !border-textColor-300"}`}>
+                        {
+                            models.map((model) => (
+                                <div
+                                    key={model.value}
+                                    onClick={() => handleModelChange(model.value)}
+                                    className={`
                                  cursor-pointer transition
                                  px-3 py-2
                                  text-nowrap
@@ -33,13 +48,15 @@ ${theme === 'light' ? 'hover:bg-textColor-100/20' : 'hover:bg-textColor-300/80'}
                                  font-medium
                                 ${theme === 'light' ? 'bg-neutral-50 hover:bg-neutral-200 text-textColor-200' : 'bg-zinc-800 hover:bg-textColor-300 text-[#EEE]'}
                             `}
-                        >
-                            {model.icon}
-                            {model.name}
-                        </div>
-                    ))
-                }
-            </div>
+                                >
+                                    {model.icon}
+                                    {model.name}
+                                </div>
+                            ))
+                        }
+                    </div>
+                )
+            }
         </div>
     );
 };
