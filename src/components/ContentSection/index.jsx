f49ee04a2fd40aea7b8064c14593edf693a605c0
achraffawzi <achraffawzi224a@gmail.com>
2026-01-25 20:49:07 +0700

    speed up the upload process by moving the progress bar to 3% after 10s from uploading

--- src/components/ContentSection/index.jsx
+++ src/components/ContentSection/index.jsx
@@ -750,6 +750,16 @@ const ContentSection = ({
                 return [...newSources, ...prev];
             });
 
+            // speed up the upload process by moving the progress bar to 3% after 10s-20s from uploading
+            setTimeout(() => {
+                setKnowledgeBase(prev => prev.map(item => {
+                    if (item.progress === 0) {
+                        return { ...item, progress: 3, step: "Video pre-processing..." };
+                    }
+                    return item;
+                }));
+            }, 10000);
+
             const { uploaded_data } = await makeApiRequest("/upload", "post", formData, { 'Content-type': "multipart/form-data" });
 
             // ----------  Update knowledge base ----------
