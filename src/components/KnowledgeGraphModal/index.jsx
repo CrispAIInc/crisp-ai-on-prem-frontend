ba801c29e281138df8165a8938338adf692feed4
achraffawzi <achraffawzi224a@gmail.com>
2026-02-18 16:08:40 +0000

    handle download Json structure

--- src/components/KnowledgeGraphModal/index.jsx
+++ src/components/KnowledgeGraphModal/index.jsx
@@ -1,11 +1,12 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import Modal from 'react-bootstrap/Modal';
 import { MainContext } from '../../contexts/mainContext';
 
-import ReactJson from "@uiw/react-json-view";
+import AutoAwesomeIcon from '@mui/icons-material/AutoAwesome';
 
 import DataObjectIcon from '@mui/icons-material/DataObject';
 import JsonViewer from '../JsonViewer';
+import RippleButton from '../RippleButton';
 
 const data = {
     "entities": [
@@ -281,6 +282,30 @@ const KnowledgeGraphModal = ({ show, onHide }) => {
 
     const { theme } = useContext(MainContext);
 
+    const [isDownloading, setIsDownloading] = useState(false);
+
+    const handleDownload = () => {
+        setIsDownloading(true);
+        try {
+            const jsonString = JSON.stringify(data, null, 2); // formatted
+            const blob = new Blob([jsonString], { type: "application/json" });
+            const url = URL.createObjectURL(blob);
+
+            const link = document.createElement("a");
+            link.href = url;
+            link.download = "json-structure.json";
+            document.body.appendChild(link);
+            link.click();
+
+            document.body.removeChild(link);
+            URL.revokeObjectURL(url);
+        } catch (error) {
+            console.log(error);
+        } finally {
+            setIsDownloading(false);
+        }
+    };
+
     return (
         <Modal
             show={show}
@@ -290,30 +315,29 @@ const KnowledgeGraphModal = ({ show, onHide }) => {
             className="graph-modal p-0 flex-1"
         >
             <Modal.Header closeButton className={`${theme === 'light' ? '' : 'bg-textColor-300 text-white !border-b-textColor-200'}`}>
-                <Modal.Title id="contained-modal-title-vcenter">
+                <Modal.Title id="contained-modal-title-vcenter" className="flex items-center justify-between w-full">
                     <div className="flex items-center gap-1">
                         <DataObjectIcon className={`${theme === 'dark' && 'text-textColor-200'}`} fontSize="large" />
                         <p>JSON Structure</p>
+                    </div>
+                    <div>
+                        <RippleButton cssClasses='flex items-center gap-1 disabled:cursor-not-allowed p-2'
+                            disabled={isDownloading} onClick={handleDownload}>
+                            {isDownloading ? <span className="animate-customPulse">Downloading...</span> : 'Download JSON'}
+                        </RippleButton>
                     </div>
                     {/* JSON/Graph switched */}
                     {/* ... */}
                 </Modal.Title>
             </Modal.Header>
             <Modal.Body
-                className={`
-    ${theme === 'light' ? '' : 'bg-textColor-300 text-white'}
-    p-0
-  `}
+                className={`${theme === 'light' ? '' : 'bg-textColor-300 text-white'}  p-0 `}
             >
                 <div className="flex flex-col h-[60vh]">
 
                     {/* Scrollable JSON Container */}
                     <div className="flex-1 overflow-y-auto p-6">
-                        <div className={`
-        ${theme === "light" ? 'bg-[#f5f5f5]' : 'bg-[#222]'}
-        rounded-lg
-        min-h-full
-      `}>
+                        <div className={`${theme === "light" ? 'bg-[#f5f5f5]' : 'bg-[#222]'} rounded-lg min-h-full`}>
                             <JsonViewer data={data} />
                         </div>
                     </div>
