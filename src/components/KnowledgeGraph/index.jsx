c64efeec6a99d4387f642e027564d173396a12d2
achraffawzi <achraffawzi224a@gmail.com>
2026-02-18 13:59:40 +0000

    display knowledge graph modal

--- src/components/KnowledgeGraph/index.jsx
+++ src/components/KnowledgeGraph/index.jsx
@@ -4,6 +4,7 @@ import RippleButton from '../RippleButton';
 
 import AutoAwesomeIcon from '@mui/icons-material/AutoAwesome';
 import BaseHeading from '../BaseHeading';
+import KnowledgeGraphModal from '../KnowledgeGraphModal';
 
 function KnowledgeGraph() {
 
@@ -30,91 +31,95 @@ function KnowledgeGraph() {
     const handleMouseLeave = () => setTooltipVisible(false);
 
     return (
-        <div className='flex flex-col gap-1 h-full'>
-            <div className="relative w-full">
-                <div className="flex flex-col mb-2">
-                    <label className={`${theme === "dark" ? 'text-textColor-100' : 'text-textColor-200'} font-medium`}>Context prompt</label>
-                    <span className={`${theme === "dark" ? 'text-textColor-100' : 'text-textColor-200'} text-sm`}>
-                        {/* (Optional) Provide additional context or instructions to guide the JSON generation process. This can include specific themes, styles, or elements you want to see in the generated content. */}
-                        Constrain model behavior through schema-based contextual configuration.
-                    </span>
+        <>
+            <div className='flex flex-col gap-1 h-full'>
+                <div className="relative w-full">
+                    <div className="flex flex-col mb-2">
+                        <label className={`${theme === "dark" ? 'text-textColor-100' : 'text-textColor-200'} font-medium`}>Context prompt</label>
+                        <span className={`${theme === "dark" ? 'text-textColor-100' : 'text-textColor-200'} text-sm`}>
+                            {/* (Optional) Provide additional context or instructions to guide the JSON generation process. This can include specific themes, styles, or elements you want to see in the generated content. */}
+                            Constrain model behavior through schema-based contextual configuration.
+                        </span>
+                    </div>
+                    <textarea
+                        className={`w-full p-2 bg-transparent !border ${theme === "dark" ? "!border !border-textColor-200/50 rounded-md text-textColor-200" : '!border !border-textColor-100 text-textColor-300'} rounded-md resize-none focus:outline-none focus:ring-2 focus:ring-blue-500`}
+                        rows="3"
+                        placeholder='Customize your JSON structure'
+                        value={context}
+                        onChange={(e) => setContext(e.target.value)}
+                    />
                 </div>
-                <textarea
-                    className={`w-full p-2 bg-transparent !border ${theme === "dark" ? "!border !border-textColor-200/50 rounded-md text-textColor-200" : '!border !border-textColor-100 text-textColor-300'} rounded-md resize-none focus:outline-none focus:ring-2 focus:ring-blue-500`}
-                    rows="3"
-                    placeholder='Customize your JSON structure'
-                    value={context}
-                    onChange={(e) => setContext(e.target.value)}
-                />
-            </div>
-            {/* generate button */}
-            <div className='relative inline-block' onMouseMove={handleMouseMove}
-                onMouseEnter={handleMouseEnter}
-                onMouseLeave={handleMouseLeave}>
-                <RippleButton fullWidth cssClasses='flex items-center gap-1 disabled:cursor-not-allowed p-2'
-                    disabled={isGeneratingReel || checkedSourcesCount === 0 || checkedSourcesCount > MAX_SOURCES_COUNT}>
-                    {isGeneratingReel ? <><AutoAwesomeIcon color="white" className="animate-customPulse" /> <span className="animate-customPulse">Generating...</span></> : 'Generate'}
-                </RippleButton>
-                {tooltipVisible && (
-                    <p
-                        // onMouseEnter={() => setTooltipVisible(false)}
-                        className={`absolute p-2 text-sm font-semibold rounded shadow-2xl bg-background_workspace top-full ${theme === 'light' ? 'text-textColor-300' : 'text-textColor-100'}`}
-                        style={{ top: position.y, left: position.x, opacity: tooltipVisible ? 1 : 0 }}
-                    >
-                        {`Select at least one source. (max: ${MAX_SOURCES_COUNT} sources)`}
-                    </p>
-                )}
+                {/* generate button */}
+                <div className='relative inline-block' onMouseMove={handleMouseMove}
+                    onMouseEnter={handleMouseEnter}
+                    onMouseLeave={handleMouseLeave}>
+                    <RippleButton fullWidth cssClasses='flex items-center gap-1 disabled:cursor-not-allowed p-2'
+                        disabled={isGeneratingReel || checkedSourcesCount === 0 || checkedSourcesCount > MAX_SOURCES_COUNT}>
+                        {isGeneratingReel ? <><AutoAwesomeIcon color="white" className="animate-customPulse" /> <span className="animate-customPulse">Generating...</span></> : 'Generate'}
+                    </RippleButton>
+                    {tooltipVisible && (
+                        <p
+                            // onMouseEnter={() => setTooltipVisible(false)}
+                            className={`absolute p-2 text-sm font-semibold rounded shadow-2xl bg-background_workspace top-full ${theme === 'light' ? 'text-textColor-300' : 'text-textColor-100'}`}
+                            style={{ top: position.y, left: position.x, opacity: tooltipVisible ? 1 : 0 }}
+                        >
+                            {`Select at least one source. (max: ${MAX_SOURCES_COUNT} sources)`}
+                        </p>
+                    )}
+                </div>
+
+                {/* list of JSON structures */}
+                <div className="flex flex-col mt-4 mb-2 gap-2 h-full overflow-hidden">
+                    <BaseHeading text="Your composers" />
+                    <input
+                        className={`py-1 text-sm bg-transparent outline-none ${theme === 'light' ? '!border !border-textColor-100' : '!border !border-textColor-200 text-textColor-100'} w-full  rounded-full !pl-[10px]`}
+                        placeholder={"Search..."}
+                    />
+                    <div className="overflow-y-auto h-full">
+                        <p>first</p>
+                        <p>hello</p>
+                        <p>hello</p>
+                        <p>hello</p>
+                        <p>hello</p>
+                        <p>hello</p>
+                        <p>hello</p>
+                        <p>hello</p>
+                        <p>hello</p>
+                        <p>hello</p>
+                        <p>hello</p>
+                        <p>hello</p>
+                        <p>hello</p>
+                        <p>hello</p>
+                        <p>hello</p>
+                        <p>hello</p>
+                        <p>hello</p>
+                        <p>hello</p>
+                        <p>hello</p>
+                        <p>hello</p>
+                        <p>hello</p>
+                        <p>hello</p>
+                        <p>hello</p>
+                        <p>hello</p>
+                        <p>hello</p>
+                        <p>hello</p>
+                        <p>hello</p>
+                        <p>hello</p>
+                        <p>hello</p>
+                        <p>hello</p>
+                        <p>hello</p>
+                        <p>hello</p>
+                        <p>hello</p>
+                        <p>hello</p>
+                        <p>hello</p>
+                        <p>hello</p>
+                        <p>hello</p>
+                        <p>final</p>
+                    </div>
+                </div>
             </div>
 
-            {/* list of JSON structures */}
-            <div className="flex flex-col mt-4 mb-2 gap-2 h-full overflow-hidden">
-                <BaseHeading text="Your JSON structures" />
-                <input
-                    className={`py-1 text-sm bg-transparent outline-none ${theme === 'light' ? '!border !border-textColor-100' : '!border !border-textColor-200 text-textColor-100'} w-full  rounded-full !pl-[10px]`}
-                    placeholder={"Search..."}
-                />
-                <div className="overflow-y-auto h-full">
-                    <p>first</p>
-                    <p>hello</p>
-                    <p>hello</p>
-                    <p>hello</p>
-                    <p>hello</p>
-                    <p>hello</p>
-                    <p>hello</p>
-                    <p>hello</p>
-                    <p>hello</p>
-                    <p>hello</p>
-                    <p>hello</p>
-                    <p>hello</p>
-                    <p>hello</p>
-                    <p>hello</p>
-                    <p>hello</p>
-                    <p>hello</p>
-                    <p>hello</p>
-                    <p>hello</p>
-                    <p>hello</p>
-                    <p>hello</p>
-                    <p>hello</p>
-                    <p>hello</p>
-                    <p>hello</p>
-                    <p>hello</p>
-                    <p>hello</p>
-                    <p>hello</p>
-                    <p>hello</p>
-                    <p>hello</p>
-                    <p>hello</p>
-                    <p>hello</p>
-                    <p>hello</p>
-                    <p>hello</p>
-                    <p>hello</p>
-                    <p>hello</p>
-                    <p>hello</p>
-                    <p>hello</p>
-                    <p>hello</p>
-                    <p>final</p>
-                </div>
-            </div>
-        </div>
+            <KnowledgeGraphModal show={true} />
+        </>
     );
 }
 
