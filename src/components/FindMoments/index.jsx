0c51997e22813ce6c6a7baf1823ea1c3fc715b2a
achraffawzi <achraffawzi224a@gmail.com>
2026-02-28 22:48:21 +0000

    add max height for find moments input

--- src/components/FindMoments/index.jsx
+++ src/components/FindMoments/index.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext, useEffect, useRef, useState } from 'react';
 import RippleButton from '../RippleButton';
 import { MainContext } from '../../contexts/mainContext';
 
@@ -11,16 +11,33 @@ const FindMoments = () => {
     } = useContext(MainContext);
 
     const [prompt, setPrompt] = useState("");
+    const textareaRef = useRef(null);
+
+    useEffect(() => {
+        const el = textareaRef.current;
+        if (!el) return;
+
+        // Reset height to recalc
+        el.style.height = "auto";
+        el.style.height = el.scrollHeight + "px";
+
+        // Calculate 4 lines height
+        const lineHeight = parseFloat(
+            window.getComputedStyle(el).lineHeight
+        );
+        const maxSingleHeight = lineHeight * 1;
+    }, [prompt]);
 
     return (
         <div className="flex flex-col h-full  gap-2 overflow-y-hidden">
             <div className={`flex items-center gap-2 w-full pr-2 pb-2 bg-transparent !border ${theme === "dark" ? "!border !border-textColor-200/50 rounded-md text-textColor-100" : '!border !border-textColor-100 text-textColor-300'} rounded-md focus-within:ring-1 focus-within:ring-primaryColor/50`}>
                 <textarea
-                    rows={2}
+                    ref={textareaRef}
+                    rows={1}
                     value={prompt}
                     onChange={(e) => setPrompt(e.target.value)}
                     placeholder="Add more instructions for better results (optional)"
-                    className={`w-full p-2 bg-transparent resize-none focus:outline-none`}
+                    className={`w-full p-2 bg-transparent resize-none focus:outline-none overflow-y-auto max-h-40`}
                 />
                 <RippleButton cssClasses="rounded-md !py-2 !px-3 !pr-4 self-end flex items-center gap-1">
                     <SearchOutlinedIcon className={`text-white text-sm`} />
