593256e1dca4f3ad15574762ea180d51749e6a75
achraffawzi <achraffawzi224a@gmail.com>
2026-03-03 17:03:46 +0000

    scroll to top when clicking on timestamps in metadatapanel

--- src/components/MetadataPanel/index.jsx
+++ src/components/MetadataPanel/index.jsx
@@ -337,7 +337,7 @@ const MetadataPanel = ({ workspaceContainer, leftWidth, maxWidth }) => {
                                 <div>
                                   <div className="flex items-center gap-2 cursor-pointer" onClick={() => {
                                     setCurrentResource(prev => ({ ...prev, timestamp: topic.start_time }));
-                                    contentPanelContainerRef?.current.scrollTo({
+                                    metadataPanelContainer?.current.scrollTo({
                                       top: 0,
                                       behavior: "smooth", // Enables smooth scrolling
                                     });
