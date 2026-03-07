49378bc76032293383683a3eff22548fff717097
achraffawzi <achraffawzi224a@gmail.com>
2026-03-07 15:43:13 +0000

    set reels as default tab in right panel

--- src/components/ChatPanel/index.jsx
+++ src/components/ChatPanel/index.jsx
@@ -151,7 +151,7 @@ const ChatPanel = () => {
     }
   }, [showStoriesEditor, showEditor]);
 
-  const [actualTab, setActualTab] = useState("genGraph"); //genMetadata | genStories | genMedia | genGraph
+  const [actualTab, setActualTab] = useState("genMedia"); //genMetadata | genStories | genMedia | genGraph
 
   function handleTabClick(item) {
     setActualTab(item);
