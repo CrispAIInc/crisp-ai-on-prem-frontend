47dbc2dcc244ff0d4fadf16c5ef32930072c5bb5
achraffawzi <achraffawzi224a@gmail.com>
2026-03-11 13:56:28 +0000

    pass only videos to /handle-embeddings endpoint's payload for find moments feat

--- src/components/FindMoments/index.jsx
+++ src/components/FindMoments/index.jsx
@@ -108,7 +108,7 @@ const FindMoments = ({
                 `/handle-embeddings`,
                 "post",
                 JSON.stringify({
-                    sources: displayedSources?.filter(item => item?.is_checked)?.map(item => ({ source_path: item?.source_path, category: item?.category })),
+                    sources: checkedSources.filter(items => items.file_type === "video")?.map(item => ({ source_path: item?.source_path, category: item?.category })),
                 })
             );
         }
