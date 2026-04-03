986da4c60112918a9cbf1907adf78146d5cfea02
achraffawzi <achraffawzi224a@gmail.com>
2026-04-03 21:11:32 +0700

    add 'isBlog' flag to indicate that we want to geenrate a blog from te current json structure

--- src/components/ChatPanel/index.jsx
+++ src/components/ChatPanel/index.jsx
@@ -552,6 +552,7 @@ const ChatPanel = () => {
       // ============= generating json structure ==================
       setStep(STEPS[1]);
       const payload = {
+        isBlog: true,
         sources: { file_type: checkedVideoOrPdfSources[0].file_type, source_path: checkedVideoOrPdfSources[0].source_path, category: Array.isArray(checkedVideoOrPdfSources[0].category) ? checkedVideoOrPdfSources[0].category.filter(cat => cat !== "all")[0] : checkedVideoOrPdfSources[0].category },
         selectedOptions: ["graph"],
         inputContext: "",
