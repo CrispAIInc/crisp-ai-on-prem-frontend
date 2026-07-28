0dc0457fa723f5f76a58b26c128938081747fd88
achraffawzi <achraffawzi224a@gmail.com>
2026-07-29 01:01:35 +0700

    remove source title from center panel

--- src/components/CenterPanel/index.jsx
+++ src/components/CenterPanel/index.jsx
@@ -93,7 +93,7 @@ const CenterPanel = ({ messages, combinedSummary, setCombinedSummary, isCombined
             {showMetadata && (
                 <div className="w-full max-w-full">
                     <div className="flex items-center justify-between gap-3 mb-3">
-                        <BaseHeading text={`Source: ${currentResource.source_path}`} className="truncate" />
+                        {/* <BaseHeading text={`Source: ${currentResource.source_path}`} className="truncate" /> */}
                         <div className="justify-end w-fit flex items-center cursor-pointer " onClick={closeMetadataPanel} >
                             <CloseIcon className={`text-[10px] ${theme === 'light' ? 'text-textColor-300' : 'text-textColor-100'}`} />
                             <BaseHeading text="Close metadata" />
