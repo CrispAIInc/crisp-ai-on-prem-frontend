ceb8ca2c5c79eb335fdae80d0de85024ab027812
achraffawzi <achraffawzi224a@gmail.com>
2026-08-20 22:36:26 +0100

    increase width of right section of Studio

--- src/components/Studio/index.jsx
+++ src/components/Studio/index.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 function Studio() {
     return (
         <div className="max-w-[1400px] mx-auto px-6 py-6">
-            <div className="grid grid-cols-1 lg:grid-cols-[1fr_360px] gap-6 items-start">
+            <div className="grid grid-cols-1 lg:grid-cols-[1fr_550px] gap-6 items-start">
                 <p>Left</p>
                 <p>Right</p>
             </div>
