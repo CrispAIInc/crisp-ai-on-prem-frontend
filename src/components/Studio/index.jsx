d893881e660c679dafe25c5a617b1c5faa32cef2
achraffawzi <achraffawzi224a@gmail.com>
2026-08-21 21:31:38 +0100

    dynamically set active tab (right sidebar)

--- src/components/Studio/index.jsx
+++ src/components/Studio/index.jsx
@@ -1,12 +1,25 @@
-import Media from '../Media';
+import { useContext } from 'react';
+import { NAV_ITEMS } from '../../navigation/navitems';
+import { MainContext } from '../../contexts/mainContext';
 
 function Studio() {
+
+    const {
+        activeTab
+    } = useContext(MainContext);
+
+    const activeItem = NAV_ITEMS.find(
+        (item) => item.key === activeTab
+    );
+
+    const ActiveComponent = activeItem?.component;
+
     return (
         <div className="max-w-[1400px] mx-auto h-full">
             <div className="grid h-full grid-cols-1 lg:grid-cols-[1fr_400px] gap-6 items-start bg-green-600">
                 <p>Left</p>
                 <div className="h-full">
-                    <Media />
+                    {ActiveComponent && <ActiveComponent />}
                 </div>
             </div>
         </div>
