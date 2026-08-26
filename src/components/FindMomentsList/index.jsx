914c8af88798095ad7e19c97031096df79a00e0b
achraffawzi <achraffawzi224a@gmail.com>
2026-08-26 14:53:57 +0100

    DELETE /moments/id

--- src/components/FindMomentsList/index.jsx
+++ src/components/FindMomentsList/index.jsx
@@ -68,7 +68,7 @@ function FindMomentsList({ setCurrentMoment, setShowList, moments, setMoments })
     async function deleteSegment(momentId) {
         try {
             setIsSegmentDeleting(true);
-            const { success, message } = await makeApiRequest(`/moment/${momentId}`, 'DELETE');
+            const { success, message } = await makeApiRequest(`/moments/${momentId}`, 'DELETE');
 
             if (success) {
                 setMoments(prev => prev.filter(item => item.id !== momentId));
