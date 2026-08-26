b0aa8c0385a586990cb71b74a488d3f292eed5dc
achraffawzi <achraffawzi224a@gmail.com>
2026-08-26 19:21:49 +0100

    update to GET /graphs

--- src/contexts/mainContext.jsx
+++ src/contexts/mainContext.jsx
@@ -1266,7 +1266,7 @@ export default function MainProvider({ children, theme, setTheme }) {
 
     useEffect(() => {
         async function getJsonEntities() {
-            const { graphs } = await makeApiRequest('/graph');
+            const { graphs } = await makeApiRequest('/graphs');
             setJsonEntities(graphs);
         }
 
