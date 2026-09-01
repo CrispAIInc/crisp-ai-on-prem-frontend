fd08274858b0bec7cd27712ab405048f930a3c50
achraffawzi <achraffawzi224a@gmail.com>
2026-09-01 18:34:41 +0700

    remove commented code

--- src/components/MediaEntertainment/index.jsx
+++ src/components/MediaEntertainment/index.jsx
@@ -71,14 +71,6 @@ function MediaEntertainment({
 
     async function generateMedia() {
         if (isProjectReadOnly) return;
-        // if (reel.title === "") {
-        //     notify({
-        //         variant: "error",
-        //         heading: "Oops!",
-        //         subheading: "Reel title is required!",
-        //     });
-        //     return;
-        // }
         try {
             setIsReelOpen(false);
             setIsGeneratingReel(true);
