736860797f903ed2754795fa87e70bd1b9465376
achraffawzi <achraffawzi224a@gmail.com>
2026-09-02 08:24:55 +0700

    remove currentResource from global POST /discover

--- src/components/Discovery/index.jsx
+++ src/components/Discovery/index.jsx
@@ -82,7 +82,6 @@ export default function Discovery({
             const { found, additional_sources, score, timestamp, page, message, success, ...rest } = await makeApiRequest('/discover', 'POST', JSON.stringify({
                 selectedCategory,
                 searchQuestion: q,
-                currentResource,
                 selectedFormat,
                 indexes: indexes.map(item => {
                     let indexId = categoryOptions.find(idx => idx.value === item)?.id;
