97fa0323c8848e26f7f97d9acc97c86dc12e40fa
achraffawzi <achraffawzi224a@gmail.com>
2026-09-13 09:00:00 +0700

    set analytics full length flag to true by default

--- src/contexts/mainContext.jsx
+++ src/contexts/mainContext.jsx
@@ -261,7 +261,7 @@ export default function MainProvider({ children, theme, setTheme }) {
     const [timeSegmentEnd, setTimeSegmentEnd] = useState({ h: "00", m: "00", s: "00" });
     const [timeSegmentContext, setTimeSegmentContext] = useState('');
     const [timeSegmentTitle, setTimeSegmentTitle] = useState('');
-    const [timeSegmentFullLength, setTimeSegmentFullLength] = useState(false);
+    const [timeSegmentFullLength, setTimeSegmentFullLength] = useState(true);
     const [isGeneratingTimeSegment, setIsGeneratingTimeSegment] = useState(false);
     const [timeSegmentResultsDescription, setTimeSegmentResultsDescription] = useState({ start: "00:00:00", end: "00:00:00", description: "", refs: [] });
     // Find moments pane state
