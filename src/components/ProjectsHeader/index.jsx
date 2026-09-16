1c0345bbe621d1a032c26c163a7bceff5aaf0d2e
achraffawzi <achraffawzi224a@gmail.com>
2026-09-16 10:10:33 +0700

    set current_project to null when logging out from ProjectsHeader section

--- src/components/ProjectsHeader/index.jsx
+++ src/components/ProjectsHeader/index.jsx
@@ -10,6 +10,12 @@ const ProjectsHeader = () => {
   const { isSettingsModalOpen, setIsSettingsModalOpen } = useContext(ProjectContext);
   const { user } = useContext(AuthContext);
   const { logout } = useAuth();
+
+  async function handleLogout() {
+    localStorage.setItem('current_project', null);
+    await logout();
+  }
+
   return (
     <div className="flex items-center justify-between py-4 mb-6 border-b border-gray-200 bg-white/70 backdrop-blur-xl">
       <div>
@@ -18,7 +24,7 @@ const ProjectsHeader = () => {
       <UserMenu
         firstName={user.firstName}
         lastName={user.lastName}
-        onLogout={logout}
+        onLogout={handleLogout}
       />
       {
         isSettingsModalOpen && <SettingsModal hideTheme show={isSettingsModalOpen} onHide={() => setIsSettingsModalOpen(false)} />
