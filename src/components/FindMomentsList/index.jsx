a5ff76b1b8fe47502eeebf16d6f3b1837c6da87e
achraffawzi <achraffawzi224a@gmail.com>
2026-09-20 16:59:16 +0700

    add optional chaining for current moment id when deleting moment

--- src/components/FindMomentsList/index.jsx
+++ src/components/FindMomentsList/index.jsx
@@ -119,7 +119,7 @@ function MomentListItem() {
             if (success) {
                 setMoments(prev => prev.filter(item => item.id !== momentId));
                 //SET CURRENT MOMENT TO NULL IF IT'S THE ONE BEING DELETED
-                if (currentMoment.id === momentId) {
+                if (currentMoment && currentMoment?.id === momentId) {
                     setCurrentMoment(null);
                 }
                 notify({
