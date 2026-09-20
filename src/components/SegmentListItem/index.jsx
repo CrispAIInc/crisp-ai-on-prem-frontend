f3e55f1633740db6f6d4b1ce15faf0a684fcc45e
achraffawzi <achraffawzi224a@gmail.com>
2026-09-20 14:21:55 +0700

    add optional chaining for currentSegment id when deleting segment

--- src/components/SegmentListItem/index.jsx
+++ src/components/SegmentListItem/index.jsx
@@ -63,7 +63,7 @@ export default function SegmentListItem() {
             if (success) {
                 setSegmentDescriptions(prev => prev.filter(item => item.id !== segmentId));
                 // SET CURRENT SEGMENT TO NULL IF IT'S THE ONE BEING DELETED
-                if (currentSegment?.id === segmentId) {
+                if (currentSegment && currentSegment?.id === segmentId) {
                     setCurrentSegment(null);
                 }
                 notify({
